6a4a801b3cda01686fd2d920cd603c9e0ac9ec0c
dyxLike <dx01@live.cn>
2018-06-14 17:21:06 +0800

    change to 8090 add volatile demo

--- learndemo/src/main/java/dotest/thread/ThreadTest.java
+++ learndemo/src/main/java/dotest/thread/ThreadTest.java
@@ -1,16 +1,43 @@
 package dotest.thread;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThreadTest {
 
-	public static void main(String[] args) {
-		new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				while(true) {}
-			}
-		}).start();
-		
-		
-	}
+    public volatile int inc = 0;
+
+    public void increase() {
+        inc++;
+    }
+
+    /**
+     * volatile无法保证i++的原子性
+     */
+    public static void main(String[] args) {
+        final ThreadTest test = new ThreadTest();
+        ArrayList<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            threads.add(new Thread() {
+                public void run() {
+                    for (int j = 0; j < 1000; j++)
+                        test.increase();
+                }
+
+                ;
+            });
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(test.inc);
+    }
 }
