9ef1e85166e97764d32eab707496e670aba1f7c2
dyxLike <dx01@live.cn>
2018-06-21 17:19:34 +0800

    update java8Package

--- learndemo/src/main/java/dotest/java8/Lamda.java
+++ learndemo/src/main/java/dotest/java8/Lamda.java
@@ -1,15 +1,68 @@
 package dotest.java8;
 
+import org.apache.poi.ss.formula.functions.T;
+import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.IntConsumer;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 public class Lamda {
 
-    public static void main(String[] args) {
+    /**
+     * 二元算子函数
+     */
+    interface MathOperation {
+        int operation(int a, int b);
+    }
+
+    /**
+     * 函数式接口
+     */
+    interface GreetingService {
+        void sayMessage(String message);
+    }
+
+
+    @Test
+    public void listForeach() {
         List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
-// 获取对应的平方数
-        List<Integer> squaresList = numbers.stream().map(i -> i*i).distinct().collect(Collectors.toList());
+        // 获取对应的平方数
+        List<Integer> squaresList = numbers.stream().map(i -> i * i).distinct().collect(Collectors.toList());
         squaresList.stream().forEach(System.out::println);
+    }
+
+    @Test
+    public void lamda() {
+        MathOperation add = (a, b) -> a + b;
+        System.out.println(add.operation(1, 2));
+
+        MathOperation op2 = (c, b) -> c * b;
+        System.out.println(op2.operation(2,3));
+
+        GreetingService hello = msg -> System.out.println("你好, "+msg);
+        hello.sayMessage("张三");
+        GreetingService print = System.out::print;
+        print.sayMessage("李四");
+    }
+
+    String[] cars = {"轿车", "客车", "火车","跑车"};
+
+
+    @Test
+    public void ref(){
+        List<String> carList = Arrays.asList(cars);
+        carList.forEach(System.out::println);
+
+        Function<Integer,int[]> aNew = int[]::new;
+        int[] ints = aNew.apply(12);
+        System.out.println(ints.length);
+
+
+
     }
 }
---
+++ learndemo/src/main/java/dotest/java8/StreamTest.java
@@ -0,0 +1,35 @@
+package dotest.java8;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamTest {
+
+    List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
+
+    @Test
+    public void f1(){
+        strings.stream().forEach(s -> {
+            System.out.println(StringUtils.isBlank(s));
+        });
+    }
+
+    @Test
+    public void map(){
+        List<String> collect = strings.stream().filter(StringUtils::isNotBlank).map(String::toUpperCase).collect(Collectors.toList());
+        System.out.println(collect);
+    }
+
+    @Test
+    public void coll(){
+        Map<String, String> collect = strings.stream().distinct().collect(Collectors.toMap(s -> s, s -> s.toUpperCase()));
+        collect.forEach((k,v)-> System.out.println(k+"="+v));
+    }
+}
