2143f9b4ad45fe1b572f6c46233474c9639d949f
dyxLike <dx01@live.cn>
2018-06-26 17:34:45 +0800

    update threadTest

---
+++ learndemo/src/main/java/dotest/thread/CyclicBarrierTest.java
@@ -0,0 +1,49 @@
+package dotest.thread;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class CyclicBarrierTest {
+    public static void main(String[] args) {
+        int N = 4;
+        CyclicBarrier barrier = new CyclicBarrier(N, () -> System.out.println("全部写入完毕! 当前线程" + Thread.currentThread().getName()));
+
+        for (int i = 0; i < N; i++){
+            if(i==3){
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            new Writer(barrier).start();
+        }
+    }
+
+    static class Writer extends Thread {
+        private CyclicBarrier cyclicBarrier;
+
+        Writer(CyclicBarrier cyclicBarrier) {
+            this.cyclicBarrier = cyclicBarrier;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("线程" + Thread.currentThread().getName() + "正在写入数据...");
+            try {
+                Thread.sleep(5000);      //以睡眠来模拟写入数据操作
+                System.out.println("线程" + Thread.currentThread().getName() + "写入数据完毕，等待其他线程写入完毕");
+                try {
+                    cyclicBarrier.await(4,TimeUnit.SECONDS);
+                } catch (TimeoutException e) {
+                    e.printStackTrace();
+                }
+            } catch (InterruptedException | BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName()+ " 所有线程写入完毕，继续处理其他任务...");
+        }
+    }
+}
