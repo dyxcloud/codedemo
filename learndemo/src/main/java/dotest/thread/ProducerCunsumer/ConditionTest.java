c3d5807261b58835bb7f0d1cc6bba0d1261844ee
dyxLike <dx01@live.cn>
2018-07-09 09:15:12 +0800

    update threadTest

--- learndemo/src/main/java/dotest/thread/ProducerCunsumer/ConditionTest.java
+++ learndemo/src/main/java/dotest/thread/ProducerCunsumer/ConditionTest.java
@@ -1,7 +1,6 @@
 package dotest.thread.ProducerCunsumer;
 
-import java.util.Queue;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
@@ -9,11 +8,12 @@ import java.util.concurrent.locks.ReentrantLock;
 public class ConditionTest {
 
     // 用于标记两个线程的状态, 省去主动获取线程状态的麻烦
-    Thread.State consumerState = Thread.State.NEW;
-    Thread.State producerState = Thread.State.NEW;
+    volatile Thread.State consumerState = Thread.State.NEW;
+    volatile Thread.State producerState = Thread.State.NEW;
 
-    int queueSize = 10;//队列长度
-    Queue<Integer> queue = new ConcurrentLinkedQueue<>();//并发队列
+    final int queueSize = 10;//队列长度
+    volatile Stack<Integer> queue = new Stack<>();
+
     ReentrantLock consumeLock = new ReentrantLock();
     ReentrantLock produceLock = new ReentrantLock();
     Condition pFull = produceLock.newCondition();//生产者控制器
@@ -22,15 +22,15 @@ public class ConditionTest {
     Runnable consumer = () -> {
         while (true) {
             consumeLock.lock();
-            while (queue.size() == 0) {//队列空, 消费者等待
-                System.out.println("c: 等待数据");
-                consumerState = Thread.State.WAITING;
+            while (queue.isEmpty()) {//队列空, 消费者等待
                 try {
+                    System.out.println("c: 等待数据");
+                    consumerState = Thread.State.WAITING;
                     cEmpty.await();
                 } catch (InterruptedException e) { e.printStackTrace();}
             }
             consumerState = Thread.State.RUNNABLE;
-            Integer poll = queue.poll();
+            Integer poll = queue.pop();
             System.out.println("c: 获取到[" + poll + "], 剩余元素: " + queue.size());
             if(producerState==Thread.State.WAITING){
                 produceLock.lock();
@@ -45,14 +45,14 @@ public class ConditionTest {
         while(true){
             produceLock.lock();
             while(queue.size() == queueSize) {
-                System.out.println("p: 等待空间");
-                producerState = Thread.State.WAITING;
                 try {
+                    System.out.println("p: 等待空间");
+                    producerState = Thread.State.WAITING;
                     pFull.await();
                 } catch (InterruptedException e) {e.printStackTrace(); }
             }
             producerState = Thread.State.RUNNABLE;
-            queue.offer(new Random().nextInt());
+            queue.push(new Random().nextInt());
             System.out.println("p: 插入元素, 剩余空间: "+(queueSize-queue.size()));
             if(consumerState==Thread.State.WAITING){
                 consumeLock.lock();
@@ -63,17 +63,17 @@ public class ConditionTest {
         }
     };
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ConditionTest t = new ConditionTest();
         Thread consumer = new Thread(t.consumer, "consumer");
         Thread producer = new Thread(t.producer, "producer");
         consumer.start();
         producer.start();
         while(consumer.getState()!= Thread.State.WAITING || producer.getState()!= Thread.State.WAITING){
-
+            Thread.sleep(500);
         }
         // TODO 两个线程同时进入Wait状态
-        System.out.println("opps");
+        System.out.println("opps : "+consumer.getState()+".."+producer.getState());
         System.exit(0);
     }
 }
