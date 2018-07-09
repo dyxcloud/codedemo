ffa2bbf8bb8e2cfedfadced5a8448830cdf3c6fe
dyxLike <dx01@live.cn>
2018-07-09 11:43:20 +0800

    update consumer&producer

--- learndemo/src/main/java/dotest/thread/ProducerCunsumer/ConditionTest.java
+++ learndemo/src/main/java/dotest/thread/ProducerCunsumer/ConditionTest.java
@@ -1,63 +1,86 @@
 package dotest.thread.ProducerCunsumer;
 
 import java.util.*;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class ConditionTest {
 
-    // 用于标记两个线程的状态, 省去主动获取线程状态的麻烦
-    volatile Thread.State consumerState = Thread.State.NEW;
-    volatile Thread.State producerState = Thread.State.NEW;
+    private volatile Thread.State ps = Thread.State.NEW;
+    private volatile Thread.State cs = Thread.State.NEW;
 
-    final int queueSize = 10;//队列长度
-    volatile Stack<Integer> queue = new Stack<>();
+    private final int queueSize = 10;//队列长度
+    private volatile Stack<Integer> queue = new Stack<>();
+    private volatile AtomicInteger count = new AtomicInteger(0);
 
-    ReentrantLock consumeLock = new ReentrantLock();
-    ReentrantLock produceLock = new ReentrantLock();
-    Condition pFull = produceLock.newCondition();//生产者控制器
-    Condition cEmpty = consumeLock.newCondition();//消费者控制器
+    private ReentrantLock consumeLock = new ReentrantLock();
+    private ReentrantLock produceLock = new ReentrantLock();
+    private Condition pFull = produceLock.newCondition();//生产者控制器
+    private Condition cEmpty = consumeLock.newCondition();//消费者控制器
 
-    Runnable consumer = () -> {
+    private Runnable consumer = () -> {
         while (true) {
             consumeLock.lock();
-            while (queue.isEmpty()) {//队列空, 消费者等待
+            if (queue.isEmpty()) {//队列空, 消费者等待
                 try {
-                    System.out.println("c: 等待数据");
-                    consumerState = Thread.State.WAITING;
-                    cEmpty.await();
-                } catch (InterruptedException e) { e.printStackTrace();}
-            }
-            consumerState = Thread.State.RUNNABLE;
-            Integer poll = queue.pop();
-            System.out.println("c: 获取到[" + poll + "], 剩余元素: " + queue.size());
-            if(producerState==Thread.State.WAITING){
-                produceLock.lock();
-                pFull.signal();
-                produceLock.unlock();
+//                    System.out.println("c: 等待数据");
+                    if (queue.isEmpty() && ps != Thread.State.WAITING) {
+                        cs = Thread.State.WAITING;
+                        if (queue.isEmpty() && ps != Thread.State.WAITING) {
+                            cEmpty.await();
+                        }
+                    } else {
+                        if (produceLock.tryLock()) {
+                            pFull.signal();
+                            produceLock.unlock();
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                cs = Thread.State.RUNNABLE;
+                Integer poll = queue.pop();
+//                System.out.println("c: 获取到[" + poll + "], 剩余元素: " + queue.size());
+                if (produceLock.tryLock()) {
+                    pFull.signal();
+                    produceLock.unlock();
+                }
             }
             consumeLock.unlock();
         }
     };
 
-    Runnable producer = () -> {
-        while(true){
+    private Runnable producer = () -> {
+        while (true) {
             produceLock.lock();
-            while(queue.size() == queueSize) {
+            if (queue.size() == queueSize) {
                 try {
-                    System.out.println("p: 等待空间");
-                    producerState = Thread.State.WAITING;
-                    pFull.await();
-                } catch (InterruptedException e) {e.printStackTrace(); }
-            }
-            producerState = Thread.State.RUNNABLE;
-            queue.push(new Random().nextInt());
-            System.out.println("p: 插入元素, 剩余空间: "+(queueSize-queue.size()));
-            if(consumerState==Thread.State.WAITING){
-                consumeLock.lock();
-                cEmpty.signal();
-                consumeLock.unlock();
+//                    System.out.println("p: 等待空间");
+                    if (queue.size() == queueSize && cs != Thread.State.WAITING) {
+                        ps = Thread.State.WAITING;
+                        if (queue.size() == queueSize && cs != Thread.State.WAITING) {
+                            pFull.await();
+                        }
+                    } else {
+                        if (consumeLock.tryLock()) {
+                            cEmpty.signal();
+                            consumeLock.unlock();
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                ps = Thread.State.RUNNABLE;
+                queue.push(new Random().nextInt());
+                count.getAndIncrement();
+//                System.out.println("p: 插入元素, 剩余空间: " + (queueSize - queue.size()));
+                if (consumeLock.tryLock()) {
+                    cEmpty.signal();
+                    consumeLock.unlock();
+                }
             }
             produceLock.unlock();
         }
@@ -69,11 +92,14 @@ public class ConditionTest {
         Thread producer = new Thread(t.producer, "producer");
         consumer.start();
         producer.start();
-        while(consumer.getState()!= Thread.State.WAITING || producer.getState()!= Thread.State.WAITING){
-            Thread.sleep(500);
+        while (consumer.getState() != Thread.State.WAITING || producer.getState() != Thread.State.WAITING) {
+            Thread.sleep(1000);
+            System.out.println(t.count);
+            System.out.println(consumer.getState() + ".." + producer.getState());
         }
-        // TODO 两个线程同时进入Wait状态
-        System.out.println("opps : "+consumer.getState()+".."+producer.getState());
+        // 两个线程同时Waiting/blocked , 解决方式:使用多重校验来调用await()
+        System.out.println("opps : " + consumer.getState() + ".." + producer.getState());
+        System.out.println(t.count);
         System.exit(0);
     }
 }
