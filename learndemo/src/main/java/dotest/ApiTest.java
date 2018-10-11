0c91239f1ad5df93a8f51e86adcebe56ef00fc4d
dyxLike <dx01@live.cn>
2018-10-11 14:45:48 +0800

    add apitest

---
+++ learndemo/src/main/java/dotest/ApiTest.java
@@ -0,0 +1,14 @@
+package dotest;
+
+import java.util.Objects;
+
+/**
+ * @author DongYunxiang
+ * @create 2018-09-30
+ **/
+public class ApiTest {
+
+    public static void main(String[] args) {
+        System.out.println(Double.NaN+1);
+    }
+}
--- learndemo/src/main/java/show/ScreenWidth.java
+++ learndemo/src/main/java/show/ScreenWidth.java
@@ -7,10 +7,12 @@ package show;
 public class ScreenWidth {
 
     public static double getter(double srcRatio, double srcWidth){
+        System.out.print(srcRatio+":9");
         return (srcWidth*Math.sqrt(9*9+16*16))/(Math.sqrt(9*9+srcRatio*srcRatio));
     }
 
     public static void main(String[] args) {
-        System.out.println(getter(18,6));
+        System.out.println("小米8: "+getter(18.7,6.21));
+        System.out.println("Mix2s: "+getter(18,5.99));
     }
 }
