132a7c4847e1a270ee16d243751637b0e7ce5689
dyxLike <dx01@live.cn>
2018-12-12 16:46:17 +0800

    单例dcl

---
+++ learndemo/src/main/java/dotest/superTest/SingletonDclDemo.java
@@ -0,0 +1,23 @@
+package dotest.superTest;
+
+/**
+ * 单例模式demo,使用双重校验锁实现
+ *
+ * @author DongYunxiang
+ * @create 2018-12-12
+ **/
+public class SingletonDclDemo {
+
+    private static volatile SingletonDclDemo instance;
+
+    public static SingletonDclDemo getInstance() {
+        if (instance == null) {
+            synchronized (SingletonDclDemo.class) {
+                if (instance == null) {
+                    instance = new SingletonDclDemo();
+                }
+            }
+        }
+        return instance;
+    }
+}
