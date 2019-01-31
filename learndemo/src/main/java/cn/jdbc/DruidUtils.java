050d06e3ff3e00fe7cb56ecbe999f45d29d5eb67
dyxLike <dx01@live.cn>
2019-01-31 15:15:30 +0800

    druid

--- learndemo/src/main/java/cn/jdbc/DruidUtils.java
+++ learndemo/src/main/java/cn/jdbc/DruidUtils.java
@@ -1,11 +1,14 @@
 package cn.jdbc;
 
-
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import com.alibaba.druid.pool.DruidPooledConnection;
 
-import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * @author DongYunxiang
@@ -13,61 +16,26 @@ import java.sql.SQLException;
  **/
 public class DruidUtils {
 
-    private static DruidDataSource dataSource = new DruidDataSource();
+    private static DruidDataSource druidDataSource = null;
 
     static {
-        try {
-            GetDbConnectSSO();
+        Properties prop = new Properties();
+        try (InputStream in = String.class.getResourceAsStream("/druid.properties")) {
+            prop.load(in);
+            druidDataSource = (DruidDataSource) DruidDataSourceFactory.createDataSource(prop);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static DataSource getDataSource() throws Exception {
-        return dataSource;
-    }
-
-    private static void GetDbConnectSSO() throws Exception {
-        if (dataSource == null) {
-            //设置连接参数
-            dataSource.setUrl("jdbc:mysql://192.168.200.128:3306/test");
-            dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-            dataSource.setUsername("root");
-            dataSource.setPassword("123456");
-            //配置初始化大小、最小、最大
-            dataSource.setInitialSize(1);
-            dataSource.setMinIdle(1);
-            dataSource.setMaxActive(20);
-            //连接泄漏监测
-            dataSource.setRemoveAbandoned(true);
-            dataSource.setRemoveAbandonedTimeout(30);
-            //配置获取连接等待超时的时间
-            dataSource.setMaxWait(20000);
-            //配置间隔多久才进行一次检测，检测需要关闭的空闲连接，单位是毫秒
-            dataSource.setTimeBetweenEvictionRunsMillis(20000);
-            //防止过期
-            dataSource.setValidationQuery("SELECT 1");
-            dataSource.setTestWhileIdle(true);
-            dataSource.setTestOnBorrow(true);
-        }
-    }
-
-    public static Connection getConnection(){
-        Connection con = null;
-        try {
-            con = dataSource.getConnection();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new RuntimeException();
-        }
-        return con;
+    public static DruidPooledConnection getConnection() throws SQLException {
+        return druidDataSource.getConnection();
     }
 
     public static void main(String[] args) throws Exception {
-        DataSource dataSource = getDataSource();
-        for(int i =0;i<40;i++){
-            Connection con=C3p0Utils.getConnection();
-            System.out.println("获取到的"+con);
+        for (int i = 0; i < 40; i++) {
+            Connection con = DruidUtils.getConnection();
+            System.out.println("获取到的" + con);
             con.close();
         }
     }
