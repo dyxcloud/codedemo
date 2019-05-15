75e664ae6ad3fc1ad37b2bef9997d606792d30fb
dyxLike <dx01@live.cn>
2019-05-15 14:50:04 +0800

    bst iterator

--- learndemo/src/main/java/dotest/dataDeal/数据结构/BinarySearchTree.java
+++ learndemo/src/main/java/dotest/dataDeal/数据结构/BinarySearchTree.java
@@ -5,12 +5,13 @@ import org.junit.Test;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Iterator;
 
 /**
  * @author DongYunxiang
  * @create 2019-05-09
  **/
-public class BinarySearchTree<T extends Comparable<T>> {
+public class BinarySearchTree<T extends Comparable<T>> implements Iterable<Node<T>> {
 
     private Node<T> root;
 
@@ -30,6 +31,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
                 //小数, 放左边
                 if(node.left==null){
                     node.left= new Node<>(t);
+                    node.left.parent = node;
                     break;
                 }else {
                     node = node.left;
@@ -38,6 +40,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
                 //大数,放右边
                 if(node.right==null){
                     node.right=new Node<>(t);
+                    node.right.parent = node;
                     break;
                 }else {
                     node = node.right;
@@ -63,32 +66,105 @@ public class BinarySearchTree<T extends Comparable<T>> {
         return false;
     }
 
+    @Override
+    public Iterator<Node<T>> iterator() {
+        return new BstIterator();
+    }
 
+    private class BstIterator implements Iterator<Node<T>> {
+
+        Node<T> currentNode = getFirstIn();
+
+        @Override
+        public boolean hasNext() {
+            return currentNode!=null;
+        }
+
+        @Override
+        public Node<T> next() {
+            Node<T> n = currentNode;
+            currentNode = successorIn(n);
+            return n;
+        }
+
+        private Node<T> getFirstIn(){
+            if(root==null) return null;
+            Node<T> n = root;
+            while(n.left!=null){
+                n = n.left;
+            }
+            return n;
+        }
+
+        /**
+         * 搜索中序后继
+         * @param n
+         * @return
+         */
+        private Node<T> successorIn(Node<T> n) {
+            if (n == null) return null;
+            Node<T> p;
+            if (n.right != null) {
+                p = n.right;
+                while (p.left != null) {
+                    p = p.left;
+                }
+                return p;
+            } else {
+                p = n.parent;
+                while (p != null && p.left != n) {
+                    n = p;
+                    p = n.parent;
+                }
+                return p;
+            }
+        }
+    }
+
+    private BinarySearchTree<Integer> get321546(){
+        return new BinarySearchTree<Integer>(){{
+            insert(3);
+            insert(2);
+            insert(1);
+            insert(5);
+            insert(4);
+            insert(6);
+        }};
+    }
+
+    private BinarySearchTree<Integer> get123456(){
+        return new BinarySearchTree<Integer>(){{
+            insert(1);
+            insert(2);
+            insert(3);
+            insert(4);
+            insert(5);
+            insert(6);
+        }};
+    }
+
+    private BinarySearchTree<Integer> get654321(){
+        return new BinarySearchTree<Integer>(){{
+            insert(6);
+            insert(5);
+            insert(4);
+            insert(3);
+            insert(2);
+            insert(1);
+        }};
+    }
 
     @Test
     public void testinsert(){
-        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
-        tree.insert(3);
-        tree.insert(2);
-        tree.insert(1);
-        tree.insert(5);
-        tree.insert(4);
-        tree.insert(6);
-        Deque<Node> objects = new ArrayDeque<>();
+        BinarySearchTree<Integer> tree = get321546();
+        Deque<Node<Integer>> objects = new ArrayDeque<>();
         objects.push(tree.getRoot());
         TreeSearch.dfsInOrder1(tree.getRoot());
     }
 
     @Test
     public void testContains(){
-        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
-        tree.insert(3);
-        tree.insert(2);
-        tree.insert(1);
-        tree.insert(5);
-        tree.insert(4);
-        tree.insert(6);
-
+        BinarySearchTree<Integer> tree = get321546();
         TestCase.assertTrue(tree.contains(6));
         TestCase.assertTrue(tree.contains(4));
         TestCase.assertTrue(tree.contains(2));
@@ -99,31 +175,23 @@ public class BinarySearchTree<T extends Comparable<T>> {
 
     @Test
     public void testdebug(){
-        BinarySearchTree<Integer> tree0 = new BinarySearchTree<Integer>(){{
-            insert(3);
-            insert(2);
-            insert(1);
-            insert(5);
-            insert(4);
-            insert(6);
-        }};
-        BinarySearchTree<Integer> tree1 = new BinarySearchTree<Integer>(){{
-            insert(1);
-            insert(2);
-            insert(3);
-            insert(4);
-            insert(5);
-            insert(6);
-        }};
-        BinarySearchTree<Integer> tree2 = new BinarySearchTree<Integer>(){{
-            insert(6);
-            insert(5);
-            insert(4);
-            insert(3);
-            insert(2);
-            insert(1);
-        }};
+        BinarySearchTree<Integer> tree0 = get321546();
+        BinarySearchTree<Integer> tree1 = get123456();
+        BinarySearchTree<Integer> tree2 = get654321();
         System.out.println();
+    }
+
+    @Test
+    public void testIterator(){
+        BinarySearchTree<Integer> tree0 = get321546();
+        // for(Node<Integer> n: tree0){
+        //     System.out.print(n.data);
+        // }
+        // Iterator<Node<Integer>> iterator = tree0.iterator();
+        // while ((iterator.hasNext())){
+        //     System.out.print(iterator.next().data);
+        // }
+        tree0.forEach(n-> System.out.print(n.data));
     }
 
 
@@ -135,6 +203,7 @@ class Node<T extends Comparable<T>> {
      * 一个节点有四种状态: 0未遍历, 1遍历第一个数值 3完成遍历(3个数值)
      */
     int state;
+    Node<T> parent;
     Node<T> left;
     Node<T> right;
 
