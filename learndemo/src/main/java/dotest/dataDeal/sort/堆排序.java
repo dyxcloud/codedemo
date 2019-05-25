506b0d8c60bfc5164704c9a53801b5695e287f84
dyxLike <dx01@live.cn>
2019-05-25 19:25:38 +0800

    堆排-优先队列

--- learndemo/src/main/java/dotest/dataDeal/sort/堆排序.java
+++ learndemo/src/main/java/dotest/dataDeal/sort/堆排序.java
@@ -5,6 +5,9 @@ import junit.framework.TestCase;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
 /**
  * @author DongYunxiang
@@ -20,9 +23,24 @@ public class 堆排序 {
         }
     }
 
+    public void sortWithPriorityQueue(long[] arr){
+        PriorityQueue<Long> heap = LongStream.of(arr).boxed().collect(Collectors.toCollection(PriorityQueue::new));
+       int i = 0;
+       while(!heap.isEmpty()){
+           arr[i++] = heap.poll();
+       }
+    }
+
     @Test
     public void testSort(){
         sort(ArrayData.ARR);
+        System.out.println(Arrays.toString(ArrayData.ARR));
+        TestCase.assertTrue(ArrayData.isSort(ArrayData.ARR));
+    }
+
+    @Test
+    public void testsortWithPriorityQueue(){
+        sortWithPriorityQueue(ArrayData.ARR);
         System.out.println(Arrays.toString(ArrayData.ARR));
         TestCase.assertTrue(ArrayData.isSort(ArrayData.ARR));
     }
