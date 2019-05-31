33d5d8c048d8e2736616cf28b4c0c5ad358c34e7
dyxLike <dx01@live.cn>
2019-05-31 15:46:45 +0800

    计数排序

--- learndemo/src/main/java/dotest/dataDeal/sort/ArrayData.java
+++ learndemo/src/main/java/dotest/dataDeal/sort/ArrayData.java
@@ -18,13 +18,32 @@ public class ArrayData {
         ARR = random.longs(0,100).limit(10).toArray();
     }
 
+    public static int[] getIntArr(){
+        return Arrays.stream(ARR).mapToInt(l->(int)l).toArray();
+    }
+
     public static void swap(long[] arr, int i1, int i2) {
         long tmp = arr[i1];
         arr[i1] = arr[i2];
         arr[i2] = tmp;
     }
 
+    public static void swap(int[] arr,int i1, int i2){
+        int tmp = arr[i1];
+        arr[i1] = arr[i2];
+        arr[i2] = tmp;
+    }
+
     public static boolean isSort(long[] arr){
+        for(int i=1;i<arr.length;i++){
+            if(arr[i-1]>arr[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isSort(int[] arr){
         for(int i=1;i<arr.length;i++){
             if(arr[i-1]>arr[i]){
                 return false;
