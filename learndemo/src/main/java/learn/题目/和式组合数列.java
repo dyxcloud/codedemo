dd6ad36b3ea244590a0b6b1b31e5c919d3ed1a89
dyxLike <dx01@live.cn>
2019-12-05 15:45:05 +0800

    update

--- learndemo/src/main/java/learn/题目/和式组合数列.java
+++ learndemo/src/main/java/learn/题目/和式组合数列.java
@@ -1,8 +1,9 @@
 package learn.题目;
 
-import java.io.IOException;
+import org.junit.Test;
+
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @SuppressWarnings("NonAsciiCharacters")
@@ -12,37 +13,30 @@ public class 和式组合数列 {
 	 * 给定一个正整数,列出可能的数列其相加和为此整数
 	 * @date 2017年12月17日
 	 */
-	public ArrayList<List<Integer>> getNumList(int target) {
-		ArrayList<List<Integer>> list = new ArrayList<List<Integer>>();
+	ArrayList<List<Integer>> getNumList(int target) {
+		ArrayList<List<Integer>> list = new ArrayList<>();
 		if (target <= 0) {
-			list.add(new ArrayList<Integer>());
+			list.add(new ArrayList<>());
 			return list;
 		} else if (target == 1) {
-			list.add(Arrays.asList(new Integer[] { 1 }));
+			list.add(Collections.singletonList(1));
 			return list;
 		}
 		for (int i = target; i > 0; i--) {
 			ArrayList<List<Integer>> numList = getNumList(target - i);
-			for (int k = 0; k < numList.size(); k++) {
-				ArrayList<Integer> startList = new ArrayList<Integer>();
-				startList.add(i);
-				List<Integer> endList = numList.get(k);
-				if (endList.size() != 0 && endList.get(0) > i)
-					continue;
-				startList.addAll(endList);
-				list.add(startList);
-			}
+            for (List<Integer> aNumList : numList) {
+                ArrayList<Integer> startList = new ArrayList<>();
+                startList.add(i);
+                if (aNumList.size() != 0 && aNumList.get(0) > i)
+                    continue;
+                startList.addAll(aNumList);
+                list.add(startList);
+            }
 		}
 		return list;
 	}
-	
-	
-	public static double getPi(int n) {
-		double c = n*Math.tan(2*Math.PI/n);
-		return c/2;
-	}
-	
-	public static void main(String[] args) throws IOException {
-		System.out.println(getPi(999999));
-	}
+	@Test
+    public void testget(){
+	    getNumList(10).forEach(System.out::println);
+    }
 }
