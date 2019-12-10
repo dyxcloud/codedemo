478573a2f110ee64da339c4dafdbd2d1c59d69d4
dyxLike <dx01@live.cn>
2019-12-10 15:00:37 +0800

    二分查找

--- learndemo/src/main/java/learn/find/BinarySearch.java
+++ learndemo/src/main/java/learn/find/BinarySearch.java
@@ -1,25 +1,14 @@
 package learn.find;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.stream.IntStream;
+
 public class BinarySearch {
 
-	public static void main(String[] args) {
-		int[] arr = newArrays0ToN(9999999);// 生成一个0到n的顺序数组
 
-		// int index=search(arr, 5645789);//遍历查找
-		int index = binarySearch(arr, 5645789);// 二分查找
-		// int index = binarySearchByRecursion(arr, 5645789, 0, arr.length-1);//
-		// 递归二分
-
-		if (index > -1)
-			System.out.println("所在位置为:" + index);
-		else
-			System.out.println("数组中没有该元素!");
-	}
-
-	/*
-	 * 普通二分法查找有序数组中des所在的索引位置
-	 */
-	public static int binarySearch(int[] arr, int des) {
+	public int bs(int[] arr, int des) {
 		int low = 0;
 		int high = arr.length - 1;
 		while (low <= high) {
@@ -35,43 +24,27 @@ public class BinarySearch {
 		return -1;
 	}
 
-	/*
-	 * 递归二分法查找有序数组中des所在的索引位置
-	 */
-	public static int binarySearchByRecursion(int[] arr, int des, int low, int high) {
-		int mid = (low + high) / 2;
-		if (low > high || des > arr[high] || des < arr[low]) {
+	public int bsr(int[] arr, int des, int low, int high) {
+		if (low > high || des > arr[high] || des < arr[low])
 			return -1;
-		}
+		int mid = (low + high) / 2;
 		if (des > arr[mid]) {
-			return binarySearchByRecursion(arr, des, mid + 1, high);
+			return bsr(arr, des, mid + 1, high);
 		} else if (des < arr[mid]) {
-			return binarySearchByRecursion(arr, des, low, mid - 1);
+			return bsr(arr, des, low, mid - 1);
 		} else {
 			return mid;
 		}
 	}
 
-	/*
-	 * 遍历数组查找des的下标索引
-	 */
-	public static int search(int[] arr, int des) {
-		for (int i = 0; i < arr.length; i++) {
-			if (des == arr[i])
-				return i;
+	@Test
+	public void test() {
+		{
+			int[] arr = IntStream.rangeClosed(0,9999999).toArray();// 生成一个0到n的顺序数组
+			int target = 5645789;
+			Assert.assertEquals(target,bs(arr, target));
+			target = -1;
+			Assert.assertEquals(-1,bs(arr, target));
 		}
-		return -1;
 	}
-
-	/*
-	 * 生成0到n的正整数数组
-	 */
-	public static int[] newArrays0ToN(int n) {
-		int[] arr = new int[n + 1];
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = i;
-		}
-		return arr;
-	}
-
 }
---
+++ learndemo/src/main/java/learn/题目/剑指offer/n08旋转数组的最小数字.java
@@ -0,0 +1,22 @@
+package learn.题目.剑指offer;
+
+import org.junit.Test;
+
+/**
+ * @author DongYunxiang
+ * @create 2019-12-10
+ **/
+@SuppressWarnings("NonAsciiCharacters")
+public class n08旋转数组的最小数字 {
+
+
+    public int minNumberInRotateArray(int [] array) {
+
+        return 0;
+    }
+
+    @Test
+    public void test(){
+
+    }
+}
