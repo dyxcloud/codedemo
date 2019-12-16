cb20fbcc276423134bd1a3055c32dfa9a278e94e
dyxLike <dx01@live.cn>
2019-12-16 10:29:31 +0800

    offer10

--- learndemo/src/main/java/learn/题目/剑指offer/n10二进制中1的个数.java
+++ learndemo/src/main/java/learn/题目/剑指offer/n10二进制中1的个数.java
@@ -10,12 +10,19 @@ import org.junit.Test;
 @SuppressWarnings("NonAsciiCharacters")
 public class n10二进制中1的个数 {
 
+    @Deprecated
+    public int NumberOf1_(int n) {
+        int result = 0;//从低位开始,逐位判断是否为1
+        for (int flag = 1; flag != 0; flag = flag << 1) {
+            if ((n & flag) != 0) result++;
+        }
+        return result;
+    }
     public int NumberOf1(int n) {
         int result = 0;
-        int flag = 1;
-        while(flag!=0){
-            if((n&flag)!=0) result++;
-            flag = flag<<1;
+        while(n!=0){
+            n = n & (n-1);//操作后消去最低位的1
+            result++;
         }
         return result;
     }
