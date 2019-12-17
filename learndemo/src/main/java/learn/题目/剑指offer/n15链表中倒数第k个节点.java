55635647a7ef3e232cd50b0ccad3b1117fc39d79
dyxLike <dx01@live.cn>
2019-12-17 16:33:18 +0800

    offer15

--- learndemo/src/main/java/learn/题目/剑指offer/n15链表中倒数第k个节点.java
+++ learndemo/src/main/java/learn/题目/剑指offer/n15链表中倒数第k个节点.java
@@ -91,4 +91,61 @@ public class n15链表中倒数第k个节点 {
             TestCase.assertEquals(null,listNode);
         }
     }
+
+    /**
+     * 判断链表是否形成环状结构
+     */
+    public boolean isLoop(ListNode head){
+        ListNode slow = head,fast = head;
+        while(fast!=null){
+            slow = slow.next;
+            if(fast.next==null){
+                fast = null;
+            }else{
+                fast = fast.next.next;
+            }
+            if(fast==slow) return true;
+        }
+        return false;
+    }
+    @Test
+    public void testIsLoop(){
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+        ListNode n6 = new ListNode(6);
+        ListNode n7 = new ListNode(7);
+        {
+            n1.next=n2;
+            n2.next=n3;
+            n3.next=n4;
+            n4.next=n5;
+            n5.next=n6;
+            n6.next=n7;
+            n7.next=n3;
+            TestCase.assertTrue(isLoop(n1));
+        }
+        {
+            n1.next=n2;
+            n2.next=n3;
+            n3.next=n4;
+            n4.next=n5;
+            n5.next=n6;
+            n6.next=n7;
+            n7.next=n1;
+            TestCase.assertTrue(isLoop(n1));
+        }
+        {
+            n1.next=n2;
+            n2.next=n3;
+            n3.next=n4;
+            n4.next=n5;
+            n5.next=n6;
+            n6.next=n7;
+            n7.next=null;
+            TestCase.assertTrue(!isLoop(n1));
+        }
+    }
 }
