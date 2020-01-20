b5f9ea4bebb19919120803d9de69f7d894e7f9e7
dyxLike <dx01@live.cn>
2020-01-20 20:53:34 +0800

    queue

--- learndemo/src/main/java/ApiTest.java
+++ learndemo/src/main/java/ApiTest.java
@@ -1,13 +1,53 @@
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
 
 public class ApiTest  implements Serializable{
 
 
+    public static ApiTest instance = new ApiTest();
 
     public static void main(String[] args) {
-        System.out.println(Long.MAX_VALUE);
+
+        instance.receiveManual("queuename",instance::doCrawler);
+
+
+
     }
 
+    public void receiveManual(String queueName, Consumer<Object> crawlerConsumer){
+        //获取消息
+        System.out.println(queueName);
+        String text = null;
+        if(isConuinue(text,queueName)){
+            crawlerConsumer.accept(text);
+        }
+        //ack
+        //关闭连接
+    }
+
+    public boolean isConuinue(Object o, String queueName){
+        //如果不需要拦截,直接true
+        List<String> targetQueues = Arrays.asList("crawler");
+        boolean needCheck = targetQueues.contains(queueName);
+        if(!needCheck){
+            return true;
+        }
+        //判断o任务状态
+        System.out.println(o);
+        if(true){
+            return true;
+        }else {
+            //存档消息
+            System.out.println(queueName);
+            return false;
+        }
+    }
+
+    public void doCrawler(Object o){
+        System.out.println(o);
+    }
 
 
 }
