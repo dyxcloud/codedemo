e0f558362a21dc51458b1845e49a785d35d4b2dc
dyxLike <dx01@live.cn>
2020-02-17 17:57:32 +0800

    api test

--- learndemo/src/main/java/ApiTest.java
+++ learndemo/src/main/java/ApiTest.java
@@ -1,7 +1,9 @@
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.function.Consumer;
+import java.util.stream.IntStream;
 
 public class ApiTest  implements Serializable{
 
@@ -10,10 +12,23 @@ public class ApiTest  implements Serializable{
 
     public static void main(String[] args) {
 
-        instance.receiveManual("queuename",instance::doCrawler);
+        // instance.receiveManual("queuename",instance::doCrawler);
+
+        Random random = new Random();
+        IntStream.rangeClosed(0,20).forEach(i-> System.out.println(Math.round(random.nextGaussian())));
 
 
-
+    }
+    public static String bytesToHex(byte[] bytes) {
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < bytes.length; i++) {
+            String hex = Integer.toHexString(bytes[i] & 0xFF);
+            if(hex.length() < 2){
+                sb.append(0);
+            }
+            sb.append(hex);
+        }
+        return sb.toString();
     }
 
     public void receiveManual(String queueName, Consumer<Object> crawlerConsumer){
