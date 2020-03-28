ae4a0478b4bd8819adf92c679de3092b44757c9b
dyxLike <dx01@live.cn>
2020-03-28 18:29:30 +0800

    move

--- springbootdemo/src/main/java/com/example/pidservice/SpringBootRunner.java
+++ springbootdemo/src/main/java/com/example/pidservice/SpringBootRunner.java
@@ -14,29 +14,12 @@ import java.util.Scanner;
 @SpringBootApplication
 public class SpringBootRunner implements CommandLineRunner {
 
-    @Autowired
-    C1 c1;
-    @Autowired
-    C2 c2;
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRunner.class, args);
 	}
 
 	public void run(String... args) {
-        System.out.println(c1.getter1);
-        System.out.println(c2.getter1);
 
-        System.out.println(c1.getter1.equals(c2.getter1));
-
-        Scanner scanner = new Scanner(System.in);
-        List<String> list = Arrays.asList("exit", "logout", "quit");
-        while(scanner.hasNextLine()){
-            String s1 = scanner.nextLine();
-            if(list.contains(s1.toLowerCase())){
-                System.exit(0);
-            }
-        }
 	}
 
 
