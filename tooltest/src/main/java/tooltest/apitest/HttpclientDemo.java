e7c48492536b3976083f38cdfdb029c78d4de228
dyxLike <dx01@live.cn>
2020-07-05 21:14:26 +0800

    httpclient

--- tooltest/src/main/java/tooltest/apitest/HttpclientDemo.java
+++ tooltest/src/main/java/tooltest/apitest/HttpclientDemo.java
@@ -1,14 +1,15 @@
 package tooltest.apitest;
 
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 
 public class HttpclientDemo {
 
     public static void main(String[] args) {
-        DefaultHttpClient httpClient = new DefaultHttpClient();
         CloseableHttpClient aDefault = HttpClients.createDefault();
+        HttpClientBuilder.create().build();
+        HttpClients.custom().build();
     }
 
 }
