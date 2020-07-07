3d11b52827ca8eda7333e1aa9b1b110d6f5926ed
dyxLike <dx01@live.cn>
2020-07-07 21:07:45 +0800

    链表求和 反向 循环

--- learndemo/src/main/java/learn/题目/leetcode/链表求和.java
+++ learndemo/src/main/java/learn/题目/leetcode/链表求和.java
@@ -26,46 +26,177 @@ public class 链表求和 {
      * 输出：9 -> 1 -> 2，即912
      */
 
+
+    @Test
+    public void testfuncReverse() {
+        {
+            ListNode l1 = ListNode.create(3, 4, 2);
+            ListNode l2 = ListNode.create(4, 6, 5);
+            Integer[] r = {8, 0, 7};
+            TestCase.assertEquals(Arrays.toString(r), Arrays.toString(ListNode.getList(addTwoNumbersReverse(l1, l2)).toArray()));
+        }
+        {
+            ListNode l1 = ListNode.create(8, 7, 9);
+            ListNode l2 = ListNode.create(5, 8, 6);
+            Integer[] r = {1, 4, 6, 5};
+            TestCase.assertEquals(Arrays.toString(r), Arrays.toString(ListNode.getList(addTwoNumbersReverse(l1, l2)).toArray()));
+        }
+        {
+            ListNode l1 = ListNode.create(5, 4, 3, 2, 1);
+            ListNode l2 = ListNode.create(5, 4, 3, 2, 1);
+            Integer[] r = {1, 0, 8, 6, 4, 2};
+            TestCase.assertEquals(Arrays.toString(r), Arrays.toString(ListNode.getList(addTwoNumbersReverse(l1, l2)).toArray()));
+        }
+        {
+            ListNode l1 = ListNode.create(3, 2, 1);
+            ListNode l2 = ListNode.create(5, 4, 3, 2, 1);
+            Integer[] r = {5, 4, 6, 4, 2};
+            TestCase.assertEquals(Arrays.toString(r), Arrays.toString(ListNode.getList(addTwoNumbersReverse(l1, l2)).toArray()));
+        }
+        {
+            ListNode l1 = null;
+            ListNode l2 = ListNode.create(5, 4, 3, 2, 1);
+            Integer[] r = {5, 4, 3, 2, 1};
+            TestCase.assertEquals(Arrays.toString(r), Arrays.toString(ListNode.getList(addTwoNumbersReverse(l1, l2)).toArray()));
+        }
+        {
+            ListNode l1 = null;
+            ListNode l2 = null;
+            Integer[] r = {};
+            TestCase.assertEquals(Arrays.toString(r), Arrays.toString(ListNode.getList(addTwoNumbersReverse(l1, l2)).toArray()));
+        }
+        {
+            ListNode l1 = ListNode.create(0);
+            ListNode l2 = ListNode.create(0);
+            Integer[] r = {0};
+            TestCase.assertEquals(Arrays.toString(r), Arrays.toString(ListNode.getList(addTwoNumbersReverse(l1, l2)).toArray()));
+        }
+    }
+
+    public ListNode addTwoNumbersReverse(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        ArrayDeque<Integer> stack1 = new ArrayDeque<>();
+        ArrayDeque<Integer> stack2 = new ArrayDeque<>();
+        while(l1!=null){
+            stack1.offerFirst(l1.val);
+            l1 = l1.next;
+        }
+        while(l2!=null){
+            stack2.offerFirst(l2.val);
+            l2=l2.next;
+        }
+        ListNode preNode = null;
+        int incr = 0;
+        while(stack1.peekFirst()!=null||stack2.peekFirst()!=null||incr!=0){
+            int v1 = stack1.peekFirst() == null ? 0 : stack1.pollFirst();
+            int v2 = stack2.peekFirst() == null ? 0 : stack2.pollFirst();
+            int r = v1 + v2;
+            if (incr != 0) {
+                r += incr;
+                incr = 0;
+            }
+            ListNode listNode;
+            if (r > 9) {
+                listNode = new ListNode(r - 10);
+                incr = 1;
+            } else {
+                listNode = new ListNode(r);
+            }
+            listNode.next=preNode;
+            preNode = listNode;
+        }
+        return preNode;
+    }
+
     @Test
     public void testfunc() {
         {
             ListNode l1 = ListNode.create(2, 4, 3);
             ListNode l2 = ListNode.create(5, 6, 4);
             Integer[] r = {7, 0, 8};
-            TestCase.assertEquals(Arrays.toString(r), Arrays.toString(ListNode.getList(addTwoNumbers(l1, l2)).toArray()));
+            TestCase.assertEquals(Arrays.toString(r), Arrays.toString(ListNode.getList(addTwoNumbersLoop(l1, l2)).toArray()));
         }
         {
             ListNode l1 = ListNode.create(9, 7, 8);
             ListNode l2 = ListNode.create(6, 8, 5);
             Integer[] r = {5, 6, 4, 1};
-            TestCase.assertEquals(Arrays.toString(r), Arrays.toString(ListNode.getList(addTwoNumbers(l1, l2)).toArray()));
+            TestCase.assertEquals(Arrays.toString(r), Arrays.toString(ListNode.getList(addTwoNumbersLoop(l1, l2)).toArray()));
         }
         {
             ListNode l1 = ListNode.create(1, 2, 3, 4, 5);
             ListNode l2 = ListNode.create(1, 2, 3, 4, 5);
             Integer[] r = {2, 4, 6, 8, 0, 1};
-            TestCase.assertEquals(Arrays.toString(r), Arrays.toString(ListNode.getList(addTwoNumbers(l1, l2)).toArray()));
+            TestCase.assertEquals(Arrays.toString(r), Arrays.toString(ListNode.getList(addTwoNumbersLoop(l1, l2)).toArray()));
         }
         {
             ListNode l1 = ListNode.create(1, 2, 3);
             ListNode l2 = ListNode.create(1, 2, 3, 4, 5);
             Integer[] r = {2, 4, 6, 4, 5};
-            TestCase.assertEquals(Arrays.toString(r), Arrays.toString(ListNode.getList(addTwoNumbers(l1, l2)).toArray()));
+            TestCase.assertEquals(Arrays.toString(r), Arrays.toString(ListNode.getList(addTwoNumbersLoop(l1, l2)).toArray()));
         }
         {
             ListNode l1 = null;
             ListNode l2 = ListNode.create(1, 2, 3, 4, 5);
             Integer[] r = {1, 2, 3, 4, 5};
-            TestCase.assertEquals(Arrays.toString(r), Arrays.toString(ListNode.getList(addTwoNumbers(l1, l2)).toArray()));
+            TestCase.assertEquals(Arrays.toString(r), Arrays.toString(ListNode.getList(addTwoNumbersLoop(l1, l2)).toArray()));
         }
         {
             ListNode l1 = null;
             ListNode l2 = null;
             Integer[] r = {};
-            TestCase.assertEquals(Arrays.toString(r), Arrays.toString(ListNode.getList(addTwoNumbers(l1, l2)).toArray()));
+            TestCase.assertEquals(Arrays.toString(r), Arrays.toString(ListNode.getList(addTwoNumbersLoop(l1, l2)).toArray()));
+        }
+        {
+            ListNode l1 = ListNode.create(0);
+            ListNode l2 = ListNode.create(0);
+            Integer[] r = {0};
+            TestCase.assertEquals(Arrays.toString(r), Arrays.toString(ListNode.getList(addTwoNumbersLoop(l1, l2)).toArray()));
         }
     }
 
+    public ListNode addTwoNumbersLoop(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        ListNode result = null;
+        ListNode preNode = null;
+        int incr = 0;
+        while (l1 != null || l2 != null || incr != 0) {
+            int v1 = l1 == null ? 0 : l1.val;
+            int v2 = l2 == null ? 0 : l2.val;
+            int r = v1 + v2;
+            if (incr != 0) {
+                r += incr;
+                incr = 0;
+            }
+            ListNode listNode;
+            if (r > 9) {
+                listNode = new ListNode(r - 10);
+                incr = 1;
+            } else {
+                listNode = new ListNode(r);
+            }
+            if (result == null) {
+                result = listNode;
+            }
+            if(preNode!=null){
+                preNode.next = listNode;
+            }
+            preNode = listNode;
+
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        return result;
+    }
+
+    @SuppressWarnings("unused")
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         if (l1 == null && l2 == null) {
             return null;
@@ -85,7 +216,6 @@ public class 链表求和 {
             return node;
         }
     }
-
 
     @Test
     public void testCreate() {
