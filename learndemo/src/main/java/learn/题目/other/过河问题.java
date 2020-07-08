936979986b3b99840f395a05907e941710d231dd
dyxLike <dx01@live.cn>
2020-07-08 23:00:22 +0800

    过河问题

--- learndemo/src/main/java/learn/题目/other/过河问题.java
+++ learndemo/src/main/java/learn/题目/other/过河问题.java
@@ -3,6 +3,11 @@ package learn.题目.other;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
 /**
  * @author DongYunxiang
  * @create 2020-07-08
@@ -27,7 +32,11 @@ public class 过河问题 {
      */
     @Test
     public void testfunc() {
-        TestCase.assertEquals(17, func(1, 2, 5, 10));
+        // TestCase.assertEquals(17, func(1, 2, 5, 10));
+        // TestCase.assertEquals(15, func(1, 2, 5, 8));
+        // TestCase.assertEquals(5, func(1, 1, 1, 1));
+        // TestCase.assertEquals(7, func(1, 1, 1, 1, 1));
+        TestCase.assertEquals(16, func(1, 2, 2, 10));
     }
 
     public int func(int... arr) {
@@ -36,7 +45,51 @@ public class 过河问题 {
          * 最慢的两个过去
          * 对岸最快的回来
          */
-        return 0;
+        int result = 0;
+        LinkedList<Integer> to = new LinkedList<>();
+        LinkedList<Integer> from = Arrays.stream(arr).sorted().boxed().collect(Collectors.toCollection(LinkedList::new));
+        System.out.println("from: " + from.hashCode());
+        System.out.println("to: " + to.hashCode());
+        while (!from.isEmpty()) {
+            if (to.isEmpty()) {
+                int t1 = trans(from, to, true);
+                int t2 = trans(from, to, true);
+                result += Math.max(t1, t2);
+                if (!from.isEmpty()) {
+                    int i = trans(to, from, true);
+                    result += i;
+                }
+                continue;
+            }
+            if (!from.isEmpty()) {
+                int t1 = trans(from, to, false);
+                int t2 = trans(from, to, false);
+                result += Math.max(t1, t2);
+                if (!from.isEmpty()) {
+                    int i = trans(to, from, true);
+                    result += i;
+                }
+            }
+        }
+        return result;
+    }
+
+    private int trans(LinkedList<Integer> from, LinkedList<Integer> to, boolean isMin) {
+        Integer poll;
+        if (isMin) {
+            poll = from.pollFirst();
+        } else {
+            poll = from.pollLast();
+        }
+        if (poll != null) {
+            System.out.println(from.hashCode() + " trans to " + to.hashCode() + "\t" + poll);
+            to.offer(poll);
+            Collections.sort(from);
+            Collections.sort(to);
+            return poll;
+        } else {
+            return 0;
+        }
     }
 
 }
