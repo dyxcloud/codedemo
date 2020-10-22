fb511e9759daa5348c5d69ab242466731d4570a0
dyxLike <dx01@live.cn>
2020-10-22 23:10:11 +0800

    caffeine

---
+++ tooltest/src/main/java/tooltest/apitest/CaffeineDemo.java
@@ -0,0 +1,19 @@
+package tooltest.apitest;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.junit.Test;
+
+public class CaffeineDemo {
+
+
+    @Test
+    public void testfunc(){
+        Cache<String, Object> cache1 = Caffeine.newBuilder().build();
+        cache1.put("name","caffine");
+        Cache<String, Object> cache2 = Caffeine.newBuilder().build();
+        System.out.println(cache1.getIfPresent("name"));
+        System.out.println(cache2.getIfPresent("name"));
+    }
+
+}
