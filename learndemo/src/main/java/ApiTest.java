ac8b5198d0063ab2f2ec99de7f8e68ee05fb6c9a
dyxLike <dx01@live.cn>
2020-12-17 00:07:08 +0800

    size

--- learndemo/src/main/java/ApiTest.java
+++ learndemo/src/main/java/ApiTest.java
@@ -1,43 +1,28 @@
+import jdk.nashorn.internal.ir.debug.ObjectSizeCalculator;
 import org.junit.Test;
 
 import java.io.Serializable;
-import java.util.*;
 
 public class ApiTest implements Serializable {
 
 
-    @SuppressWarnings({"rawtypes", "unchecked"})
     @Test
-    public void test1() {
-        int start = 1990;
-        int end = 2020;
-        String keyname = "year";
-        List<Map<String, Object>> datas = new ArrayList<>();
-        HashMap<String, Object> map = new HashMap<>();
-        map.put(keyname, "2000");
-        datas.add(map);
-        map = new HashMap<>();
-        map.put(keyname, "2018");
-        datas.add(map);
-
-        Map[] maps = new Map[end - start + 1];
-        for (Map<String, Object> m : datas) {
-            int year = Integer.parseInt(m.get(keyname).toString());
-            maps[year - start] = m;
-        }
-
-        for (int year = start; year <= end; year++) {
-            if (maps[year - start] == null) {
-                Map<String, Object> mm = new HashMap<>();
-                mm.put(keyname, String.valueOf(year));
-                maps[year - start] = mm;
-            }
-        }
-
-        List<Map<String, Object>> maps2 = Arrays.asList(maps);
-        System.out.println(maps2.size());
-        maps2.forEach(System.out::println);
+    public void ttt() {
+        System.out.println(ObjectSizeCalculator.getObjectSize(true));
+        System.out.println(ObjectSizeCalculator.getObjectSize(1));
+        Object o = new Object();
+        System.out.println(ObjectSizeCalculator.getObjectSize(o));
+        String s = new String();
+        System.out.println(ObjectSizeCalculator.getObjectSize(s));
+        s = new String("1");
+        System.out.println(ObjectSizeCalculator.getObjectSize(s));
+        s = new String("aa");
+        System.out.println(ObjectSizeCalculator.getObjectSize(s));
+        s = new String("aaaa");
+        System.out.println(ObjectSizeCalculator.getObjectSize(s));
     }
+
+
 
 
 }
