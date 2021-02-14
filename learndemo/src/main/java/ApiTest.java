6772913288371d94046ab6db67c34073d39f150f
dyxLike <dx01@live.cn>
2021-02-14 18:04:55 +0800

    test

--- learndemo/src/main/java/ApiTest.java
+++ learndemo/src/main/java/ApiTest.java
@@ -1,28 +1,10 @@
-import jdk.nashorn.internal.ir.debug.ObjectSizeCalculator;
 import org.junit.Test;
 
-import java.io.Serializable;
-
-public class ApiTest implements Serializable {
-
+public class ApiTest {
 
     @Test
     public void ttt() {
-        System.out.println(ObjectSizeCalculator.getObjectSize(true));
-        System.out.println(ObjectSizeCalculator.getObjectSize(1));
-        Object o = new Object();
-        System.out.println(ObjectSizeCalculator.getObjectSize(o));
-        String s = new String();
-        System.out.println(ObjectSizeCalculator.getObjectSize(s));
-        s = new String("1");
-        System.out.println(ObjectSizeCalculator.getObjectSize(s));
-        s = new String("aa");
-        System.out.println(ObjectSizeCalculator.getObjectSize(s));
-        s = new String("aaaa");
-        System.out.println(ObjectSizeCalculator.getObjectSize(s));
+
     }
-
-
-
 
 }
