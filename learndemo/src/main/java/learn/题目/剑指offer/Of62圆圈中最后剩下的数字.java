e3bf2274142490a3b1e117899504421fcd5bb829
dyxLike <dx01@live.cn>
2021-02-14 12:19:49 +0800

    数组随机访问版约瑟夫环

--- learndemo/src/main/java/learn/题目/剑指offer/Of62圆圈中最后剩下的数字.java
+++ learndemo/src/main/java/learn/题目/剑指offer/Of62圆圈中最后剩下的数字.java
@@ -3,64 +3,78 @@ package learn.题目.剑指offer;
 import junit.framework.TestCase;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @SuppressWarnings("NonAsciiCharacters")
 public class Of62圆圈中最后剩下的数字 {
 
-
-    // 数数版: 时间复杂度 n*m
-    // 优化1: 省略超过size的计数
-    public int lastRemaining(int n, int m) {
-        int runTimes = 0;
-        LinkedList<Integer> list = IntStream.range(0, n).boxed().collect(Collectors.toCollection(LinkedList::new));
-        Iterator<Integer> iterator = list.iterator();
-        int useM = getUsedM(list.size(), m);//优化1:
-        int count = 1;
-        for (; ; ) {
-            while (iterator.hasNext()) {
-                runTimes++;
-                Integer next = iterator.next();
-                if (count % useM == 0) {
-                    iterator.remove();
-                    if (list.isEmpty()) {
-                        System.out.println("runTimes: " + runTimes);
-                        return next;
+    class Linked {
+        // 数数版: 时间复杂度 n*m
+        // 优化1: 省略超过size的计数
+        public int lastRemaining(int n, int m) {
+            int runTimes = 0;
+            LinkedList<Integer> list = IntStream.range(0, n).boxed().collect(Collectors.toCollection(LinkedList::new));
+            Iterator<Integer> iterator = list.iterator();
+            int useM = getUsedM(list.size(), m);//优化1:
+            int count = 1;
+            for (; ; ) {
+                while (iterator.hasNext()) {
+                    runTimes++;
+                    Integer next = iterator.next();
+                    if (count % useM == 0) {
+                        iterator.remove();
+                        if (list.isEmpty()) {
+                            System.out.println("runTimes: " + runTimes);
+                            return next;
+                        }
+                        count = 1;
+                        useM = getUsedM(list.size(), m);
+                    } else {
+                        count++;
                     }
-                    count = 1;
-                    useM = getUsedM(list.size(), m);
-                } else {
-                    count++;
                 }
+                iterator = list.iterator();
             }
-            iterator = list.iterator();
+        }
+
+        private int getUsedM(int size, int m) {
+            if (size <= 1) {
+                return 1;
+            }
+            if (m <= size) {
+                return m;
+            }
+            int result = m % size;
+            if (result == 0) {
+                return m;
+            }
+            return result;
         }
     }
 
-    private int getUsedM(int size, int m) {
-        if (size <= 1) {
-            return 1;
+    class Arrayed {
+        //使用计算下标随机访问的方式
+        public int lastRemaining(int n, int m) {
+            ArrayList<Integer> list = IntStream.range(0, n).boxed().collect(Collectors.toCollection(ArrayList::new));
+            int index = 0;
+            while (n > 1) {
+                index = (index + m - 1) % n;
+                list.remove(index);//使用System.arraycopy进行数组元素批量复制, 避免for循环来移动元素, 时间复杂度远小于O(n)
+                n--;
+            }
+            return list.get(0);
         }
-        if (m <= size) {
-            return m;
-        }
-        int result = m % size;
-        if (result == 0) {
-            return m;
-        }
-        return result;
     }
+
 
     @Test
     public void testLast() {
-        TestCase.assertEquals(3, lastRemaining(5, 3));
-        TestCase.assertEquals(2, lastRemaining(10, 17));
-        TestCase.assertEquals(64165, lastRemaining(70866, 116922));
+        Arrayed arrayed = new Arrayed();
+        TestCase.assertEquals(3, arrayed.lastRemaining(5, 3));
+        TestCase.assertEquals(2, arrayed.lastRemaining(10, 17));
+        TestCase.assertEquals(64165, arrayed.lastRemaining(70866, 116922));
     }
 
     /**
