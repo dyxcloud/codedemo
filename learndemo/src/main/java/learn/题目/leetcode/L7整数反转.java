e16f72fce5a10169075b5dc8b81ceaa045568599
dyxLike <dx01@live.cn>
2021-02-19 14:23:51 +0800

    l7整数反转

---
+++ learndemo/src/main/java/learn/题目/leetcode/L7整数反转.java
@@ -0,0 +1,36 @@
+package learn.题目.leetcode;
+
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author DongYunxiang
+ * @create 2021-02-19
+ **/
+public class L7整数反转 {
+
+    public int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            result = result * 10 + pop;
+        }
+        return result;
+    }
+
+    @Test
+    public void tt() {
+        TestCase.assertEquals(321, reverse(123));
+        TestCase.assertEquals(-321, reverse(-123));
+        TestCase.assertEquals(21, reverse(120));
+        TestCase.assertEquals(0, reverse(0));
+    }
+}
