2cd849dab2a5a46767e9aae9d76abcc08a19c72a
dyxLike <dx01@live.cn>
2021-02-20 13:53:30 +0800

    L21合并两个有序链表 非递归

--- learndemo/src/main/java/learn/题目/leetcode/L21合并两个有序链表.java
+++ learndemo/src/main/java/learn/题目/leetcode/L21合并两个有序链表.java
@@ -15,19 +15,52 @@ import java.util.List;
 public class L21合并两个有序链表 {
 
 
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    public ListNode mergeTwoLists0(ListNode l1, ListNode l2) {
         if (l1 == null) {
             return l2;
         } else if (l2 == null) {
             return l1;
         }
         if (l2.val < l1.val) {
-            l2.next = mergeTwoLists(l1, l2.next);
+            l2.next = mergeTwoLists0(l1, l2.next);
             return l2;
         } else {
-            l1.next = mergeTwoLists(l1.next, l2);
+            l1.next = mergeTwoLists0(l1.next, l2);
             return l1;
         }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        }
+        ListNode root;
+        if (l1.val > l2.val) {
+            root = l2;
+            l2 = l2.next;
+        } else {
+            root = l1;
+            l1 = l1.next;
+        }
+        ListNode current = root;
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
+                current.next = l2;
+                l2 = l2.next;
+            } else {
+                current.next = l1;
+                l1 = l1.next;
+            }
+            current = current.next;
+        }
+        if (l1 == null) {
+            current.next = l2;
+        } else {
+            current.next = l1;
+        }
+        return root;
     }
 
     @Test
