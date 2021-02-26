3cf5d0e36ac7de4be5c4032113da3b3e17fcfe25
dyxLike <dx01@live.cn>
2021-02-26 22:36:45 +0800

    L67二进制求和 初始

---
+++ learndemo/src/main/java/learn/题目/leetcode/L67二进制求和.java
@@ -0,0 +1,76 @@
+package learn.题目.leetcode;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class L67二进制求和 {
+
+    public String addBinary(String a, String b) {
+        char[] longChars, shortChars;
+        if (a.length() > b.length()) {
+            longChars = a.toCharArray();
+            shortChars = b.toCharArray();
+        } else {
+            shortChars = a.toCharArray();
+            longChars = b.toCharArray();
+        }
+        int pre = 0;
+        for (int is = shortChars.length - 1, il = is + (longChars.length - shortChars.length);
+             is >= 0;
+             is--, il--) {
+            int s = shortChars[is] == '1' ? 1 : 0;
+            int l = longChars[il] == '1' ? 1 : 0;
+            int sum = pre + s + l;
+            switch (sum) {
+                case 0:
+                    pre = 0;
+                    longChars[il] = '0';
+                    break;
+                case 1:
+                    pre = 0;
+                    longChars[il] = '1';
+                    break;
+                case 2:
+                    pre = 1;
+                    longChars[il] = '0';
+                    break;
+                case 3:
+                    pre = 1;
+                    longChars[il] = '1';
+                    break;
+            }
+        }
+        for (int il = longChars.length - shortChars.length - 1; il >= 0; il--) {
+            int l = longChars[il] == '1' ? 1 : 0;
+            int sum = pre + l;
+            switch (sum) {
+                case 0:
+                    pre = 0;
+                    longChars[il] = '0';
+                    break;
+                case 1:
+                    pre = 0;
+                    longChars[il] = '1';
+                    break;
+                case 2:
+                    pre = 1;
+                    longChars[il] = '0';
+                    break;
+            }
+        }
+        if (pre == 1) {
+            char[] result = new char[longChars.length + 1];
+            result[0] = '1';
+            System.arraycopy(longChars, 0, result, 1, longChars.length);
+            return new String(result);
+        } else {
+            return new String(longChars);
+        }
+    }
+
+    @Test
+    public void ttt() {
+        TestCase.assertEquals("100", addBinary("11", "1"));
+        TestCase.assertEquals("10101", addBinary("1010", "1011"));
+    }
+}
