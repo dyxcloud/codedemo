86ab3ec76298e7088cf0363f00682e96eb0e2503
dyxLike <dx01@live.cn>
2021-03-02 16:55:16 +0800

    L70爬楼梯

---
+++ learndemo/src/main/java/learn/题目/leetcode/L70爬楼梯.java
@@ -0,0 +1,42 @@
+package learn.题目.leetcode;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.IntUnaryOperator;
+
+/**
+ * @author DongYunxiang
+ * @create 2021-03-02
+ **/
+public class L70爬楼梯 {
+
+    Map<Integer, Integer> cache = new HashMap<>();
+
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        Integer result = cache.get(n);
+        if (result != null) {
+            return result;
+        } else {
+            result = climbStairs(n - 1) + climbStairs(n - 2);
+            cache.put(n, result);
+            return result;
+        }
+    }
+
+
+    @Test
+    public void tt() {
+        IntUnaryOperator func = this::climbStairs;
+        TestCase.assertEquals(2, func.applyAsInt(2));
+        TestCase.assertEquals(3, func.applyAsInt(3));
+    }
+}
