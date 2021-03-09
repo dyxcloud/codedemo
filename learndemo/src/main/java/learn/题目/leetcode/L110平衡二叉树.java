ff5a5bdc3cfcff9b3511bfee4c51756cd295dcfc
dyxLike <dx01@live.cn>
2021-03-09 17:29:25 +0800

    L110平衡二叉树

--- learndemo/src/main/java/learn/题目/leetcode/L110平衡二叉树.java
+++ learndemo/src/main/java/learn/题目/leetcode/L110平衡二叉树.java
@@ -12,10 +12,18 @@ public class L110平衡二叉树 {
 
     public boolean isBalanced(TreeNode root) {
         if (root == null) return true;
-        if (root.left == null && root.right == null) return true;
-        if (root.left == null && (root.right.left != null || root.right.right != null)) return false;
-        if (root.right == null && (root.left.left != null || root.left.right != null)) return false;
+        if (Math.abs(maxDepth(root.left) - maxDepth(root.right)) > 1) return false;
         return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 
     @Test
@@ -37,3 +45,5 @@ public class L110平衡二叉树 {
         }
     }
 }
+
+
