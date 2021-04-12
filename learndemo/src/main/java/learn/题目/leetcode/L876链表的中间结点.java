656a917227e8050f629c42058b7c52e2c894d9bd
dyxLike <dx01@live.cn>
2021-04-12 17:13:15 +0800

    L876链表的中间结点

---
+++ learndemo/src/main/java/learn/题目/leetcode/L876链表的中间结点.java
@@ -0,0 +1,47 @@
+package learn.题目.leetcode;
+
+import junit.framework.TestCase;
+import learn.题目.剑指offer.ListNode;
+import org.junit.Test;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ * @author DongYunxiang
+ * @create 2021-04-12
+ **/
+public class L876链表的中间结点 {
+    public ListNode middleNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode fast = head;
+        while (fast != null) {
+            if (fast.next == null) {
+                break;
+            }
+            fast = fast.next.next;
+            head = head.next;
+        }
+        return head;
+    }
+
+    @Test
+    public void ttt() {
+        Function<ListNode, ListNode> f = this::middleNode;
+        {
+            ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+            TestCase.assertEquals(3, f.apply(head).val);
+        }
+        {
+            ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))));
+            TestCase.assertEquals(4, f.apply(head).val);
+        }
+        {
+            ListNode head = new ListNode(1);
+            TestCase.assertEquals(1, f.apply(head).val);
+        }
+    }
+
+}
