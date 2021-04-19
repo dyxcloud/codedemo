14f7a6a9cd2387f8ab21f8b43842465f006add2b
dyxLike <dx01@live.cn>
2021-04-19 21:01:19 +0800

    L234回文链表

--- learndemo/src/main/java/learn/题目/leetcode/L234回文链表.java
+++ learndemo/src/main/java/learn/题目/leetcode/L234回文链表.java
@@ -5,7 +5,6 @@ import learn.题目.剑指offer.ListNode;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.function.Predicate;
 
 /**
@@ -33,9 +32,33 @@ public class L234回文链表 {
 
     public boolean isPalindrome(ListNode head) {
         if (head == null || head.next == null) return true;
-
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // System.out.println(slow);
+        ListNode dummy = new ListNode(-1);
+        while (slow != null) {
+            ListNode newOne = slow;
+            slow = slow.next;
+            ListNode tmp = dummy.next;
+            dummy.next = newOne;
+            newOne.next = tmp;
+        }
+        slow = dummy.next;
+        while (head != null && slow != null) {
+            if (head.val != slow.val) {
+                return false;
+            }
+            head = head.next;
+            slow = slow.next;
+        }
         return true;
     }
+
+    //方式3: 遍历链表 生成2个数字 一个正序生成一个倒序生成 看是否相等
 
     @Test
     public void ttt() {
@@ -46,6 +69,10 @@ public class L234回文链表 {
         }
         {
             ListNode root = ListNode.creatList(1, 2, 2, 1);
+            TestCase.assertEquals(true, f.test(root));
+        }
+        {
+            ListNode root = ListNode.creatList(1, 2, 3, 2, 1);
             TestCase.assertEquals(true, f.test(root));
         }
         {
