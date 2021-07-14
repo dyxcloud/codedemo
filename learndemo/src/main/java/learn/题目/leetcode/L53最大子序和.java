e792f0a8c4782f8f8365a35a7b49f68e268794cf
dyxLike <dx01@live.cn>
2021-07-14 22:09:54 +0800

    L53最大子序和 dp改进

--- learndemo/src/main/java/learn/题目/leetcode/L53最大子序和.java
+++ learndemo/src/main/java/learn/题目/leetcode/L53最大子序和.java
@@ -7,6 +7,8 @@ import java.util.function.ToIntFunction;
 
 public class L53最大子序和 {
 
+    int count = 0;
+
     //O(n^2)
     public int maxSubArray0(int[] nums) {
         int max = -100001;
@@ -14,6 +16,7 @@ public class L53最大子序和 {
             int sum = 0;
             for (int r = l; r < nums.length; r++) {
                 sum += nums[r];
+                count++;
                 if (sum > max) {
                     max = sum;
                 }
@@ -24,21 +27,20 @@ public class L53最大子序和 {
 
     
     /*
-    	-2	1	-3	4	-1	2	1	-5  终止
-    -5  /   /   /   /   /   /   /   -5
-    1	/	/	/	/	/	/	1	1
-    2	/	/	/	/	/	2	3	3
-    -1	/   /   /   /   -1	2	3	3				
-    4	/   /   /	4	3	5	6	6		
-    -3	/   /	-3  4   3   5   6	6					
-    1	/	1   -2  4   3   5   6	6					
-    -2	-2  1   -2  4   3   5   6	6						
-    起始								
+    	-2	1	-3	4	-1	2	1	-5  40  终止j
+    40  /   /   /   /   /   /   /   /   40
+    -5  /   /   /   /   /   /   /   -5  40
+    1	/	/	/	/	/	/	1	1   40
+    2	/	/	/	/	/	2	3	3   40
+    -1	/   /   /   /   -1	2	3	3   40
+    4	/   /   /	4	4	5	6	6   41
+    -3	/   /	-3  4   4   5   6	6   41
+    1	/	1   1   4   4   5   6	6   41
+    -2	-2  1   1   4   4   5   6	6	41					
+    起始i
 
-max(当前坐标 , 上边, 左边)
-i起始
-j终止
 max(sum(i~j) , f(i+1,j), f(i,j-1))
+//这个公式无法利用子调用的结果直接求解,还要重新累加, 所以比暴力法还慢
      */
 
     //DP
@@ -57,20 +59,43 @@ max(sum(i~j) , f(i+1,j), f(i,j-1))
         if (cache[start][end] != 0) {
             return cache[start][end];
         }
+        int sub1 = maxSubArrayHelper(nums, start + 1, end);
+        int sub2 = maxSubArrayHelper(nums, start, end - 1);
         //计算当前起始的值
         int current = 0;
         for (int i = start; i <= end; i++) {
             current += nums[i];
         }
-        System.out.println(current + " ," + start + "~" + end);
-        int sub1 = maxSubArrayHelper(nums, start + 1, end);
-        int sub2 = maxSubArrayHelper(nums, start, end - 1);
+        // System.out.println(current + " ," + start + "~" + end);
         int result = Math.max(current, Math.max(sub1, sub2));
         cache[start][end] = result;
         return result;
     }
 
     //f(i)=max{f(i−1)+nums[i],nums[i]}
+    int[] cache2;
+
+    public int maxSubArray20(int[] nums) {
+        cache2 = new int[nums.length];
+        int result = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            result = Math.max(maxSubArray20Helper(nums, i), result);
+        }
+        return result;
+    }
+
+    public int maxSubArray20Helper(int[] nums, int end) {
+        if (end < 0) return 0;
+        if (end == 0) return nums[0];
+        if (cache2[end] != 0) {
+            return cache2[end];
+        }
+        count++;
+        int result = Math.max(maxSubArray20Helper(nums, end - 1) + nums[end], nums[end]);
+        cache2[end] = result;
+        return result;
+    }
+
     public int maxSubArray2(int[] nums) {
         int pre = 0, maxAns = nums[0];
         for (int x : nums) {
@@ -82,12 +107,14 @@ max(sum(i~j) , f(i+1,j), f(i,j-1))
 
     @Test
     public void ttt() {
-        ToIntFunction<int[]> func = this::maxSubArray2;
+        ToIntFunction<int[]> func = this::maxSubArray;
         TestCase.assertEquals(6, func.applyAsInt(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+        TestCase.assertEquals(41, func.applyAsInt(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 40}));
         TestCase.assertEquals(1, func.applyAsInt(new int[]{1}));
         TestCase.assertEquals(0, func.applyAsInt(new int[]{0}));
         TestCase.assertEquals(-1, func.applyAsInt(new int[]{-1}));
         TestCase.assertEquals(-100000, func.applyAsInt(new int[]{-100000}));
+        System.out.println(count);
     }
 
 }
