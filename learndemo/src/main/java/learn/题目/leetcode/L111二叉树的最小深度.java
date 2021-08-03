5ce56190ae20aeee527b394a0a190fac9d476785
dyxLike <dx01@live.cn>
2021-08-03 21:20:08 +0800

    最小深度 广度优先

--- learndemo/src/main/java/learn/题目/leetcode/L111二叉树的最小深度.java
+++ learndemo/src/main/java/learn/题目/leetcode/L111二叉树的最小深度.java
@@ -4,8 +4,9 @@ import junit.framework.TestCase;
 import learn.题目.剑指offer.TreeNode;
 import org.junit.Test;
 
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.function.ToIntFunction;
 
 public class L111二叉树的最小深度 {
 
@@ -15,53 +16,54 @@ public class L111二叉树的最小深度 {
             return 0;
         if (root.left == null && root.right == null)
             return 1;
-        if (root.left == null)
-            return 1 + minDepth(root.right);
-        if (root.right == null)
-            return 1 + minDepth(root.left);
-        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+        int deep = Integer.MAX_VALUE;
+        if (root.right != null)
+            deep = Math.min(deep, minDepth(root.right));
+        if (root.left != null)
+            deep = Math.min(deep, minDepth(root.left));
+        return 1 + deep;
     }
 
-    //TODO 深度优先中序 迭代法
-    public int minDepth1(TreeNode root) {
-        if (root == null)
-            return 0;
-        int min = 100000;
-        int currentFloor = 0;
-        Deque<TreeNode> stack = new LinkedList<>();
-        stack.addFirst(root);
-        while (!stack.isEmpty()) {
-            TreeNode first = stack.removeFirst();
-            if (first != null) {
-                currentFloor++;
-                if (first.left == null && first.right == null) {
-                    min = Math.min(min, currentFloor);
-                    currentFloor--;
-                    continue;
-                }
-                stack.addFirst(first.right);
-                stack.addFirst(first.left);
-            } else {
-
+    //广度优先
+    //关联 L104二叉树的最大深度
+    public int minDepthBfs(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        int floor = 1;
+        int currentCount = 1;
+        int nextCount = 0;
+        while (!queue.isEmpty()) {
+            TreeNode poll = queue.poll();
+            if (poll.left == null && poll.right == null) {
+                break;
+            }
+            if (poll.right != null) {
+                queue.offer(poll.right);
+                nextCount++;
+            }
+            if (poll.left != null) {
+                queue.offer(poll.left);
+                nextCount++;
+            }
+            currentCount--;
+            if (currentCount == 0) {
+                floor++;
+                currentCount = nextCount;
+                nextCount = 0;
             }
         }
-        return min;
-    }
-
-    //TODO 广度优先
-
-    interface Func {
-        int minDepth(TreeNode root);
+        return floor;
     }
 
     @Test
     public void tt() {
-        Func f = this::minDepth1;
+        ToIntFunction<TreeNode> f = this::minDepthBfs;
         {
             TreeNode root = new TreeNode(3
                     , new TreeNode(9)
                     , new TreeNode(20, new TreeNode(15), new TreeNode(7)));
-            TestCase.assertEquals(2, f.minDepth(root));
+            TestCase.assertEquals(2, f.applyAsInt(root));
         }
         {
             TreeNode root = new TreeNode(2
@@ -69,20 +71,20 @@ public class L111二叉树的最小深度 {
                     , null, new TreeNode(4
                     , null, new TreeNode(5
                     , null, new TreeNode(6)))));
-            TestCase.assertEquals(5, f.minDepth(root));
+            TestCase.assertEquals(5, f.applyAsInt(root));
         }
         {
             TreeNode root = new TreeNode(1
                     , new TreeNode(2, new TreeNode(4), new TreeNode(5))
                     , new TreeNode(3));
-            TestCase.assertEquals(2, f.minDepth(root));
+            TestCase.assertEquals(2, f.applyAsInt(root));
         }
         {
-            TestCase.assertEquals(0, f.minDepth(null));
+            TestCase.assertEquals(0, f.applyAsInt(null));
         }
         {
             TreeNode root = new TreeNode(1);
-            TestCase.assertEquals(1, f.minDepth(root));
+            TestCase.assertEquals(1, f.applyAsInt(root));
         }
     }
 
