942a2f02b7d4d40bb8ad455c632b3de4ee68536a
dyxLike <dx01@live.cn>
2021-08-03 16:57:16 +0800

    dp

--- learndemo/src/main/java/learn/题目/leetcode/L121买卖股票的最佳时机.java
+++ learndemo/src/main/java/learn/题目/leetcode/L121买卖股票的最佳时机.java
@@ -3,6 +3,8 @@ package learn.题目.leetcode;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.ToIntFunction;
 import java.util.stream.Stream;
 
@@ -31,7 +33,7 @@ public class L121买卖股票的最佳时机 {
         return result;
     }
 
-    
+
     /**
      * 暴力分析法, 先获取最大值和最小值
      * 判断两个值的先后顺序, 如果先小后大就直接返回
@@ -40,6 +42,7 @@ public class L121买卖股票的最佳时机 {
     public int maxProfit0(int[] prices) {
         return maxProfit0Helper(prices, 0, prices.length - 1);
     }
+
     public int maxProfit0Helper(int[] prices, int start, int end) {
         if (end - start < 1) {
             return 0;
@@ -76,10 +79,68 @@ public class L121买卖股票的最佳时机 {
         }
     }
 
+    
+        
+    /*
+    DP法
+        7   1   5   3   6   4
+    6   x   x   x   x   x   0
+    3   x   x   x   x   3   3
+    5   x   x   x   0   1   1
+    1   x   x   4   4   5   5
+    7   x   0   0   0   0   0
+     */
+
+    Map<Integer, Integer> cache;
+
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+        cache = new HashMap<>(prices.length * 4 / 3 + 1);
+        int result = 0;
+        for (int i = prices.length - 2; i >= 0; i--) {
+            int tmp = maxProfitHelper(prices, i);
+            if (tmp > result) {
+                result = tmp;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * start买入时的最优解
+     */
+    private int maxProfitHelper(int[] prices, int start) {
+        if (start == prices.length - 1) {
+            return 0;
+        }
+        if (cache.get(start) != null) {
+            return cache.get(start);
+        }
+        int c = prices[start + 1] - prices[start];
+        int result = Math.max(maxProfitHelper(prices, start + 1) + c, 0);
+        cache.put(start, result);
+        return result;
+    }
+
+    /**
+     * 最优解
+     */
+    public int maxProfit2(int[] prices) {
+        if(prices.length<=1)
+            return 0;
+        int min=prices[0],max=0;
+        for(int i=1;i<prices.length;i++){
+            max=Math.max(max,prices[i]-min);
+            min=Math.min(min,prices[i]);
+        }
+        return max;
+    }
 
     @Test
     public void tt() {
-        ToIntFunction<int[]> f = this::maxProfit0;
+        ToIntFunction<int[]> f = this::maxProfit;
         TestCase.assertEquals(5, f.applyAsInt(new int[]{7, 1, 5, 3, 6, 4}));
         TestCase.assertEquals(0, f.applyAsInt(new int[]{7, 6, 4, 3, 1}));
         TestCase.assertEquals(0, f.applyAsInt(new int[]{}));
