eb608932b0f87f16282ef8b9442fa6006bebfb2f
dyxLike <dx01@live.cn>
2021-08-11 10:36:00 +0800

    CompletableFuture

---
+++ learndemo/src/main/java/jdk/thread/CompletableFutureTest.java
@@ -0,0 +1,25 @@
+package jdk.thread;
+
+import org.junit.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
+public class CompletableFutureTest {
+    
+    @Test
+    public void tt() throws InterruptedException, ExecutionException {
+        Supplier<String> task = ()-> {
+            System.out.println("开启执行任务");
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException ignored) {}
+            return "Hello World";
+        };
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(task);
+        TimeUnit.SECONDS.sleep(3);
+        System.out.println(future.get());
+    }
+}
