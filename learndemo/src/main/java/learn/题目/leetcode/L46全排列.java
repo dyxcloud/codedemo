c42286fdafe717638c55dea6c1e057fd5cab0221
dyxLike <dx01@live.cn>
2021-08-20 17:08:42 +0800

    L46全排列

--- learndemo/src/main/java/learn/题目/leetcode/L46全排列.java
+++ learndemo/src/main/java/learn/题目/leetcode/L46全排列.java
@@ -11,61 +11,67 @@ import java.util.stream.Collectors;
 @SuppressWarnings("NonAsciiCharacters")
 public class L46全排列 {
 
-    /*
-    以字典序生成全排列
+    /**
+     * 以字典序生成全排列
      */
-    L31下一个排列 l31 = new L31下一个排列();
+    static class l46_1 {
+        L31下一个排列 l31 = new L31下一个排列();
 
-    public List<List<Integer>> permute0(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> result = new ArrayList<>();
-        int times = 1, length = nums.length;
-        while (length > 1) {
-            times *= length;
-            length--;
-        }
-        result.add(Arrays.stream(nums).boxed().collect(Collectors.toList()));
-        while (times > 1) {
-            l31.nextPermutation(nums);
-            result.add(Arrays.stream(nums).boxed().collect(Collectors.toList()));
-            times--;
-        }
-        return result;
-    }
-
-    /*
-    回溯法
-     */
-    List<List<Integer>> result;
-    int length;
-
-    public List<List<Integer>> permute(int[] nums) {
-        length = nums.length;
-        result = new ArrayList<>();
-        List<Integer> queue = new ArrayList<>(length);
-        List<Integer> sources = Arrays.stream(nums).boxed().collect(Collectors.toList());
-        helper(queue, sources);
-        return result;
-    }
-
-    private void helper(List<Integer> queue, List<Integer> sources) {
-        int qSize = queue.size();
-        for (int i = 0; i < sources.size(); i++) {
-            Integer remove = sources.remove(i);
-            queue.add(remove);
-            if (queue.size() == length) {
-                result.add(new ArrayList<>(queue));
-            } else {
-                helper(queue, sources);
+        public List<List<Integer>> permute(int[] nums) {
+            Arrays.sort(nums);
+            List<List<Integer>> result = new ArrayList<>();
+            int times = 1, length = nums.length;
+            while (length > 1) {
+                times *= length;
+                length--;
             }
-            sources.add(i, remove);
-            queue.remove(qSize);
+            result.add(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+            while (times > 1) {
+                l31.nextPermutation(nums);
+                result.add(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+                times--;
+            }
+            return result;
         }
     }
+
+    /**
+     * 回溯法
+     */
+    static class l46_2 {
+        
+        List<List<Integer>> result;
+        int length;
+
+        public List<List<Integer>> permute(int[] nums) {
+            length = nums.length;
+            result = new ArrayList<>();
+            List<Integer> queue = new ArrayList<>(length);
+            List<Integer> sources = Arrays.stream(nums).boxed().collect(Collectors.toList());
+            helper(queue, sources);
+            return result;
+        }
+
+        private void helper(List<Integer> queue, List<Integer> sources) {
+            int qSize = queue.size();
+            for (int i = 0; i < sources.size(); i++) {
+                Integer remove = sources.remove(i);
+                queue.add(remove);
+                if (queue.size() == length) {
+                    result.add(new ArrayList<>(queue));
+                } else {
+                    helper(queue, sources);
+                }
+                sources.add(i, remove);
+                queue.remove(qSize);
+            }
+        }
+    }
+
 
     @Test
     public void tt() {
-        Function<int[], List<List<Integer>>> func = this::permute;
+        Function<int[], List<List<Integer>>> func = new l46_2()::permute;
         {
             Set<List<Integer>> set = Set.of(
                     Arrays.asList(1, 2, 3),
