3102aadd69abf705711380dae6c2485401eae1f0
dyxLike <dx01@live.cn>
2021-08-23 16:02:57 +0800

    L33搜索旋转排序数组

--- learndemo/src/main/java/learn/题目/leetcode/L33搜索旋转排序数组.java
+++ learndemo/src/main/java/learn/题目/leetcode/L33搜索旋转排序数组.java
@@ -3,13 +3,15 @@ package learn.题目.leetcode;
 import junit.framework.TestCase;
 import org.junit.Test;
 
-import java.text.MessageFormat;
 import java.util.function.ToIntBiFunction;
 
 @SuppressWarnings("NonAsciiCharacters")
 public class L33搜索旋转排序数组 {
 
-    public int search(int[] nums, int target) {
+    /**
+     * 坐标重映射的二分查找
+     */
+    public int search0(int[] nums, int target) {
         //二分找旋转点
         int start;
         if (nums[0] < nums[nums.length - 1]) {
@@ -48,6 +50,92 @@ public class L33搜索旋转排序数组 {
             return l;
         if (target == nums[r])
             return r;
+        return -1;
+    }
+
+    /**
+     * 二分查找 分区 不重映射坐标
+     */
+    public int search1(int[] nums, int target) {
+        //二分找旋转点
+        int start;
+        if (nums[0] < nums[nums.length - 1]) {
+            start = 0;
+        } else {
+            int l = 0, r = nums.length - 1;
+            while (r - l > 1) {
+                int m = (r - l) / 2 + l;
+                if (nums[m] < nums[l]) {
+                    r = m;
+                } else {
+                    l = m;
+                }
+            }
+            start = r;
+        }
+        //二分查找
+        int l, r;
+        if (start == 0) {
+            l = 0;
+            r = nums.length - 1;
+        } else {
+            if (target >= nums[0]) {
+                l = 0;
+                r = start - 1;
+            } else {
+                l = start;
+                r = nums.length - 1;
+            }
+        }
+        while (r - l > 1) {
+            if (target == nums[l])
+                return l;
+            if (target == nums[r])
+                return r;
+            int m = (r - l) / 2 + l;
+            if (target > nums[m]) {
+                l = m;
+            } else if (target < nums[m]) {
+                r = m;
+            } else {
+                return m;
+            }
+        }
+        if (target == nums[l])
+            return l;
+        if (target == nums[r])
+            return r;
+        return -1;
+    }
+
+    /**
+     * 二分查找 一次
+     */
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (r - l > 1) {
+            if (target == nums[l]) return l;
+            if (target == nums[r]) return r;
+            int m = (r - l) / 2 + l;
+            if (target == nums[m]) return m;
+            if (nums[m] < nums[nums.length - 1]) {
+                //左侧旋转
+                if (nums[m] < target && target < nums[nums.length - 1]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            } else {
+                //右侧旋转
+                if (nums[0] < target && target < nums[m]) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            }
+        }
+        if (target == nums[l]) return l;
+        if (target == nums[r]) return r;
         return -1;
     }
 
