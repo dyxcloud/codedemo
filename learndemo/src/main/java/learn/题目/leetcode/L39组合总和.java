bb40977cdf9f2cec8eb2d83c74ebd2c77e1f0c25
dyxLike <dx01@live.cn>
2021-08-31 11:12:26 +0800

    L39组合总和

---
+++ learndemo/src/main/java/learn/题目/leetcode/L39组合总和.java
@@ -0,0 +1,73 @@
+package learn.题目.leetcode;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.*;
+import java.util.function.BiFunction;
+
+@SuppressWarnings("NonAsciiCharacters")
+public class L39组合总和 {
+
+    List<List<Integer>> result;
+    int[] candidates;
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        result = new ArrayList<>();
+        this.candidates = candidates;
+        Arrays.sort(this.candidates);
+        helper(new ArrayList<>(), target, candidates.length - 1);
+        return result;
+    }
+
+    public void helper(ArrayList<Integer> current, int currentTarget, int pickIndex) {
+        if (current.size() >= 149) {
+            return;
+        }
+        for (int i = pickIndex; i >= 0; i--) {
+            if (candidates[i] > currentTarget) {
+                continue;
+            }
+            int nextTarget = currentTarget - candidates[i];
+            if (nextTarget > 0) {
+                current.add(candidates[i]);
+                helper(current, nextTarget, i);
+                current.remove(current.size() - 1);
+            } else if (nextTarget == 0) {
+                current.add(candidates[i]);
+                result.add(new ArrayList<>(current));
+                current.remove(current.size() - 1);
+            }
+        }
+    }
+
+    @Test
+    public void tt() {
+        BiFunction<int[], Integer, List<List<Integer>>> func = this::combinationSum;
+        {
+            List<List<Integer>> apply = func.apply(new int[]{2, 3, 6, 7}, 7);
+            apply.forEach(Collections::sort);
+            TestCase.assertEquals(Set.of(Collections.singletonList(7), Arrays.asList(2, 2, 3)), new HashSet<>(apply));
+        }
+        {
+            List<List<Integer>> apply = func.apply(new int[]{2, 3, 5}, 8);
+            apply.forEach(Collections::sort);
+            TestCase.assertEquals(Set.of(Arrays.asList(2, 2, 2, 2), Arrays.asList(2, 3, 3), Arrays.asList(3, 5)), new HashSet<>(apply));
+        }
+        {
+            List<List<Integer>> apply = func.apply(new int[]{2}, 1);
+            apply.forEach(Collections::sort);
+            TestCase.assertEquals(Collections.EMPTY_SET, new HashSet<>(apply));
+        }
+        {
+            List<List<Integer>> apply = func.apply(new int[]{1}, 1);
+            apply.forEach(Collections::sort);
+            TestCase.assertEquals(Set.of(Collections.singletonList(1)), new HashSet<>(apply));
+        }
+        {
+            List<List<Integer>> apply = func.apply(new int[]{1}, 2);
+            apply.forEach(Collections::sort);
+            TestCase.assertEquals(Set.of(Arrays.asList(1, 1)), new HashSet<>(apply));
+        }
+    }
+}
