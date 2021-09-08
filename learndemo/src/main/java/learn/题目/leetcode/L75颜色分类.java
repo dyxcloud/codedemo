add78617dd94773d2ffb158d3a6dc9e55ba6963e
dyxLike <dx01@live.cn>
2021-09-08 10:19:24 +0800

    L75颜色分类

--- learndemo/src/main/java/learn/题目/leetcode/L75颜色分类.java
+++ learndemo/src/main/java/learn/题目/leetcode/L75颜色分类.java
@@ -30,7 +30,7 @@ public class L75颜色分类 {
     /**
      * 计数法 O(n)
      */
-    public void sortColors(int[] nums) {
+    public void sortColors1(int[] nums) {
         if (nums.length < 2) {
             return;
         }
@@ -45,6 +45,35 @@ public class L75颜色分类 {
                 nums[index++] = i;
             }
         }
+    }
+
+    /**
+     * 双指针 O(n) 一次遍历
+     * 主要难点: i与save2交换后,交换到i点的值无法保证, 还需要重新校验
+     */
+    public void sortColors(int[] nums) {
+        if (nums.length < 2) {
+            return;
+        }
+        int save0 = 0;
+        int save2 = nums.length - 1;
+        for (int i = 0; i <= save2; ) {
+            if (nums[i] == 0) {
+                swap(nums, i, save0++);
+                i++;
+            } else if (nums[i] == 2) {
+                swap(nums, i, save2--);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        if (i == j) return;
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 
     @Test
@@ -66,9 +95,24 @@ public class L75颜色分类 {
             assertArrayEquals(new int[]{0, 1, 2}, data);
         }
         {
+            int[] data = {0, 0, 1};
+            func.accept(data);
+            assertArrayEquals(new int[]{0, 0, 1}, data);
+        }
+        {
+            int[] data = {2, 1, 2};
+            func.accept(data);
+            assertArrayEquals(new int[]{1, 2, 2}, data);
+        }
+        {
             int[] data = {0};
             func.accept(data);
             assertArrayEquals(new int[]{0}, data);
+        }
+        {
+            int[] data = {2, 2};
+            func.accept(data);
+            assertArrayEquals(new int[]{2, 2}, data);
         }
         {
             int[] data = {1};
