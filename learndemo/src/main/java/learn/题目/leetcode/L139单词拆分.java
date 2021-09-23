571f0a075aa71890421c3e3bd1c2a450297e1079
dyxLike <dx01@live.cn>
2021-09-23 09:44:15 +0800

    回溯+缓存

--- learndemo/src/main/java/learn/题目/leetcode/L139单词拆分.java
+++ learndemo/src/main/java/learn/题目/leetcode/L139单词拆分.java
@@ -14,41 +14,49 @@ public class L139单词拆分 {
      * 从句子起点尝试拼接单词,成功向后继续拼接,失败就回溯
      */
     public boolean wordBreak0(String s, List<String> wordDict) {
+        cache = new Boolean[s.length()];
         wordDict.sort((s1, s2) -> s2.length() - s1.length());
         return test(s.toCharArray(), 0, wordDict);
     }
 
+    Boolean[] cache;
+
+    /**
+     * 加入缓存, 表示start起始的字符串是bad的, 这个缓存会从后向前慢慢生成, 直到第0位
+     */
     private boolean test(char[] chars, int start, List<String> dict) {
         if (start >= chars.length) {
             return true;
         }
+        if (cache[start] != null) return cache[start];
         for (String word : dict) {
-            boolean notMatch = false;
+            boolean match = true;
             for (int i = 0; i < word.length(); i++) {
                 if (start + i >= chars.length
                         || word.charAt(i) != chars[start + i]) {
-                    notMatch = true;
+                    match = false;
                     break;
                 }
             }
-            if (!notMatch) {
+            if (match) {
                 boolean test = test(chars, start + word.length(), dict);
                 if (test) {
                     return true;
                 }
             }
         }
+        cache[start] = false;
         return false;
     }
 
-    
+
     public boolean wordBreak(String s, List<String> wordDict) {
         return false;
     }
-    
+
     @Test
     public void tt() {
-        BiPredicate<String, List<String>> func = this::wordBreak;
+        BiPredicate<String, List<String>> func = this::wordBreak0;
         Assertions.assertTrue(func.test("leetcode", Arrays.asList("leet", "code")));
         Assertions.assertTrue(func.test("applepenapple", Arrays.asList("apple", "pen")));
         Assertions.assertFalse(func.test("catsandog", Arrays.asList("cats", "dog", "sand", "and", "cat")));
