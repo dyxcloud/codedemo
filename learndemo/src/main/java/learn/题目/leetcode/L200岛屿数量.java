531ed63d4b9ead493e471413006bd071ef632521
dyxLike <dx01@live.cn>
2021-09-28 15:30:25 +0800

    并查集

--- learndemo/src/main/java/learn/题目/leetcode/L200岛屿数量.java
+++ learndemo/src/main/java/learn/题目/leetcode/L200岛屿数量.java
@@ -3,42 +3,15 @@ package learn.题目.leetcode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.ToIntFunction;
 
 @SuppressWarnings("NonAsciiCharacters")
 public class L200岛屿数量 {
 
-    public int numIslands0(char[][] grid) {
-        List<Integer> newX = new ArrayList<>();
-        int count = 0;
-        for (int y = 0; y < grid.length; y++) {
-            for (int x = 0; x < grid[0].length; x++) {
-                if (grid[y][x] != '0') {
-                    if (x - 1 < 0 || grid[y][x - 1] == '0') {//左边空
-                        if (y - 1 < 0 || grid[y - 1][x] == '0') {//上边空
-                            count++;
-                            newX.add(x);
-                        }
-                    } else {//左边不空
-                        if (y - 1 >= 0 && grid[y - 1][x] != '0') {//上边不空
-                            if (newX.contains(x)) {
-                                count--;
-                                newX.remove(Integer.valueOf(x));
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return count;
-    }
-
     /**
-     * 搜索标记方式
+     * 搜索标记方式 DFS
      */
-    public int numIslands(char[][] grid) {
+    public int numIslands0(char[][] grid) {
         boolean[][] walkMap = new boolean[grid.length][grid[0].length];
         int count = 0;
         for (int y = 0; y < grid.length; y++) {
@@ -64,6 +37,67 @@ public class L200岛屿数量 {
                 walk(grid, walkMap, nx, ny);
             }
         }
+    }
+
+
+    /**
+     * 并查集 QuickUnion Rank 路径减半
+     */
+    int lengthX;
+    int[] parents;
+    int[] ranks;
+    int count;
+
+    public int numIslands(char[][] grid) {
+        //初始化并查集
+        lengthX = grid[0].length;
+        parents = new int[lengthX * grid.length];
+        ranks = new int[lengthX * grid.length];
+        count = 0;
+        for (int i = 0; i < parents.length; i++) {
+            parents[i] = i;
+            ranks[i] = 1;
+        }
+        //进行遍历合并
+        for (int y = 0; y < grid.length; y++) {
+            for (int x = 0; x < grid[0].length; x++) {
+                if (grid[y][x] == '1') {
+                    count++;
+                    //左边或者上边非空,就合并
+                    if (x - 1 >= 0 && grid[y][x - 1] == '1') {
+                        union(y * lengthX + x, y * lengthX + x - 1);
+                    }
+                    if (y - 1 >= 0 && grid[y - 1][x] == '1') {
+                        union(y * lengthX + x, (y - 1) * lengthX + x);
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    private void union(int v1, int v2) {
+        if (v1 == v2) return;
+        int p1 = find(v1);
+        int p2 = find(v2);
+        if (p1 == p2) return;
+        if (ranks[p1] > ranks[p2]) {
+            parents[p2] = p1;
+        } else if (ranks[p1] < ranks[p2]) {
+            parents[p1] = p2;
+        } else {
+            parents[p1] = p2;
+            ranks[p2]++;
+        }
+        count--;
+    }
+
+    private int find(int v) {
+        while (v != parents[v]) {
+            parents[v] = parents[parents[v]];//路径减半
+            v = parents[v];
+        }
+        return v;
     }
 
     @Test
@@ -103,6 +137,21 @@ public class L200岛屿数量 {
                     {'1', '1', '1'}
             };
             Assertions.assertEquals(1, func.applyAsInt(grid));
+        }
+        {
+            char[][] grid = {
+                    {'1', '1', '1', '1', '1', '0', '1', '1', '1', '1'},
+                    {'1', '0', '1', '0', '1', '1', '1', '1', '1', '1'},
+                    {'0', '1', '1', '1', '0', '1', '1', '1', '1', '1'},
+                    {'1', '1', '0', '1', '1', '0', '0', '0', '0', '1'},
+                    {'1', '0', '1', '0', '1', '0', '0', '1', '0', '1'},
+                    {'1', '0', '0', '1', '1', '1', '0', '1', '0', '0'},
+                    {'0', '0', '1', '0', '0', '1', '1', '1', '1', '0'},
+                    {'1', '0', '1', '1', '1', '0', '0', '1', '1', '1'},
+                    {'1', '1', '1', '1', '1', '1', '1', '1', '0', '1'},
+                    {'1', '0', '1', '1', '1', '1', '1', '1', '1', '0'}
+            };
+            Assertions.assertEquals(2, func.applyAsInt(grid));
         }
     }
 }
